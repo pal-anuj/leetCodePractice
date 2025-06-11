b3813d5b4ccc16bc1e607864255be408f635ecd2
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2025-06-11 17:43:48 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/189-RotateArray.java
@@ -0,0 +1,23 @@
+// Last updated: 11/06/2025, 17:43:46
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+    }
+
+    public void reverse(int[] nums, int low, int high)
+    {
+        while (low < high)
+        {
+            int temp= nums[low];
+            nums[low] = nums[high]; 
+            nums[high] = temp;
+            low++;
+            high--;
+        }
+    }
+
+}
