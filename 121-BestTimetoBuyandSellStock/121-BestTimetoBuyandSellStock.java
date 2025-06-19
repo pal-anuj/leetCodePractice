7589f150534b8ccb480c7282f8e7b09b366f73ea
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2025-06-19 13:13:30 +0530

    File updated at 19/06/2025, 13:13:30

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,15 +1,20 @@
-// Last updated: 19/06/2025, 13:05:58
+// Last updated: 19/06/2025, 13:13:30
 class Solution {
     public int maxProfit(int[] prices) {
         int profit=0;
         int minBuyPrice=prices[0];
         for(int i =1;i<prices.length;i++)
         {
-            if(prices[i]>prices[i-1])
-                minBuyPrice= Math.min(minBuyPrice, prices[i-1]);   
+            if(prices[i] > minBuyPrice)
+            {    
+                //Keep update max Profit
                 profit = Math.max(profit, prices[i]-minBuyPrice);
-            }
+            }// if next price is low then will update it minBuyPrice
+            else
+                minBuyPrice=prices[i];
+        }        
         return profit;
 
     }
 }
+            
