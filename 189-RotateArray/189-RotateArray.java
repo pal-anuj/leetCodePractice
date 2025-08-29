f5f7b59475903ef81e67394c28c7360ee9eab5bb
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2025-08-29 22:28:43 +0530

    File updated at 29/08/2025, 22:28:42

--- 189-RotateArray/189-RotateArray.java
+++ 189-RotateArray/189-RotateArray.java
@@ -1,23 +1,22 @@
-// Last updated: 11/06/2025, 17:43:46
+// Last updated: 29/08/2025, 22:28:42
 class Solution {
-    public void rotate(int[] nums, int k) {
-        int n = nums.length;
+    public void rotate(int[] arr, int k) {
+        int n=arr.length;
         k %= n;
-        reverse(nums, 0, n-1);
-        reverse(nums, 0, k-1);
-        reverse(nums, k, n-1);
+        
+        reverse(arr, 0, n-1);
+        reverse(arr, 0, k-1);
+        reverse(arr, k, n-1);
     }
 
-    public void reverse(int[] nums, int low, int high)
+    public void reverse(int[] arr, int low, int high)
     {
-        while (low < high)
-        {
-            int temp= nums[low];
-            nums[low] = nums[high]; 
-            nums[high] = temp;
+        while(low<high){
+            int temp= arr[low];    
+            arr[low]= arr[high];
+            arr[high]= temp;
             low++;
             high--;
         }
     }
-
 }
