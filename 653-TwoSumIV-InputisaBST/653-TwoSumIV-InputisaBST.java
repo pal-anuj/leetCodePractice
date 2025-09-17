c94482cb9dd3bfb44cfe20a310b162ebc79e52be
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2025-09-17 09:51:51 +0530

    Create 653-TwoSumIV-InputisaBST

---
+++ 653-TwoSumIV-InputisaBST/653-TwoSumIV-InputisaBST.java
@@ -0,0 +1,37 @@
+// Last updated: 17/09/2025, 09:51:50
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+
+        Set<Integer> set= new HashSet<>();
+        return findTarget2(root, k, set);
+        
+    }
+
+    public boolean findTarget2(TreeNode root, int k, Set<Integer> set){
+        if(root==null)
+            return false;
+
+        if(set.contains(k-root.val))
+            return true;
+
+        set.add(root.val);    
+
+        //DFS -> calling recusion with left element || right element
+        return findTarget2(root.left, k,set) || findTarget2(root.right, k, set);
+    }
+}
