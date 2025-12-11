c6c6cafe2cd283f262323e6bc2e2155522902d4c
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2025-12-11 09:57:12 +0530

    Create 2552-MaximumSumOfDistinctSubarraysWithLengthK

---
+++ 2552-MaximumSumOfDistinctSubarraysWithLengthK/2552-MaximumSumOfDistinctSubarraysWithLengthK.java
@@ -0,0 +1,31 @@
+// Last updated: 11/12/2025, 09:57:11
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        long maxSum=0;
+        long currSum=0;
+        HashMap<Integer,Integer> hm= new HashMap<>(k);
+        int left=0;
+        for(int i=0;i<nums.length;i++){
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+            currSum += nums[i];
+            // System.out.println(currSum);
+            if(i - left + 1 >k){
+                int rem= nums[left];
+                currSum -= rem;
+                int f= hm.get(rem);
+                if(f==1)
+                    hm.remove(rem);
+                else
+                    hm.put(rem, f-1);
+
+                left++;
+            }
+            if(i - left+ 1 == k){
+                if(hm.size()==k)
+                    maxSum= Math.max(maxSum, currSum);
+            }
+        }
+        
+        return maxSum;
+    }
+}
