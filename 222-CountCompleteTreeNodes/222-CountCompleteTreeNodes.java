02a2c244479746f5e8ffc0fa7fa231dd4ea0d736
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2025-12-12 08:20:32 +0530

    File updated at 12/12/2025, 08:20:32

--- 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
+++ 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
@@ -1,4 +1,4 @@
-// Last updated: 12/12/2025, 08:01:09
+// Last updated: 12/12/2025, 08:20:32
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -17,8 +17,32 @@
 16class Solution {
 17    public int countNodes(TreeNode root) {
 18        if(root==null) return 0;
-19        int l= countNodes(root.left);
-20        int r= countNodes(root.right);
-21        return 1+l+r;
-22    }
-23}
+19        // int l= countNodes(root.left);
+20        // int r= countNodes(root.right);
+21        // return 1+l+r;
+22        
+23        int lh= leftTreeHeight(root);
+24        int rh= rightTreeHeight(root);
+25        if(lh==rh) return (1 << lh) - 1;
+26        else return 1+(countNodes(root.left)+ countNodes(root.right));
+27    }
+28
+29    public int leftTreeHeight(TreeNode root){
+30        int c=0;
+31        while(root!=null)
+32        {
+33            c++;
+34            root=root.left;
+35        }
+36        return c;
+37    }
+38    public int rightTreeHeight(TreeNode root){
+39        int c=0;
+40        while(root!=null)
+41        {
+42            c++;
+43            root=root.right;
+44        }
+45        return c;
+46    }
+47}
