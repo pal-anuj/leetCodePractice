c89dc8cd7b8f3ca2bea26e4a98fa667d3295b7f8
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2025-12-13 23:56:46 +0530

    File updated at 13/12/2025, 23:56:46

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -1,17 +1,12 @@
-// Last updated: 13/12/2025, 23:55:00
+// Last updated: 13/12/2025, 23:56:46
 1class Solution {
 2    public int removeElement(int[] nums, int val) {
 3        int n= nums.length;
-4        int[] arr= new int[n];
-5        int count=0;
-6        for(int i=0;i<n;i++){
-7            if(nums[i]!=val)
-8                arr[count++]=nums[i];
-9        }
-10
-11        for(int i=0;i<n;i++)
-12            nums[i]= arr[i];
-13        return count;
-14        
-15    }
-16}
+4        int count=0;
+5        for(int i=0;i<n;i++){
+6            if(nums[i]!=val)
+7                nums[count++]=nums[i];
+8        }
+9        return count;
+10    }
+11}
