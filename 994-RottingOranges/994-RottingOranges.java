6d5f231dad3eaf74ab99ec06867f797599b1d623
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2025-12-13 23:42:53 +0530

    File updated at 13/12/2025, 23:42:52

--- 994-RottingOranges/994-RottingOranges.java
+++ 994-RottingOranges/994-RottingOranges.java
@@ -1,4 +1,4 @@
-// Last updated: 13/12/2025, 23:38:03
+// Last updated: 13/12/2025, 23:42:52
 1class Solution {
 2
 3    public boolean isSafe(int nx, int ny, int n, int m){
@@ -15,59 +15,60 @@
 14
 15        // counter of elapsed time
 16        int elapsedTime= 0;
-17
-18        // push all rotten oranges to Queue
-19        for(int i=0;i<n;i++){
-20            for(int j=0;j<m;j++){
-21                if(grid[i][j]==2){
-22                    q.add(new int[]{i,j});
-23                }
-24            }
-25        }
-26
-27        // direction for all 4 adjacent cells
-28        int direction[][]= {{0,-1},{0,1},{-1,0},{1,0}};
-29        
-30        // perform multi source BFS
-31        while(!q.isEmpty()){
-32            boolean flag=false;
-33            int size= q.size();
-34
-35            // process all orange at current level
-36            for(int i=0;i<size;i++){
-37                int[] cell= q.poll();
-38                int x= cell[0];
-39                int y= cell[1];
+17        int freshOrange=0;
+18        
+19        // push all rotten oranges to Queue
+20        for(int i=0;i<n;i++){
+21            for(int j=0;j<m;j++){
+22                if(grid[i][j]==2){
+23                    q.add(new int[]{i,j});
+24                }else if(grid[i][j]==1)
+25                    freshOrange++;
+26            }
+27        }
+28
+29        // direction for all 4 adjacent cells
+30        int direction[][]= {{0,-1},{0,1},{-1,0},{1,0}};
+31        
+32        // perform multi source BFS
+33        while(!q.isEmpty()){
+34            boolean flag=false;
+35            int size= q.size();
+36
+37            // process all orange at current level
+38            for(int i=0;i<size;i++){
+39                int[] cell= q.poll();
 40
 41                // check all 4 direction
 42                for(int[] dir : direction){
-43                    int nx= x+ dir[0];
-44                    int ny= y+ dir[1];
+43                    int nx= cell[0]+ dir[0];
+44                    int ny= cell[1]+ dir[1];
 45
 46                    //if cell is safe and has fresh orange
 47                    if(isSafe(nx, ny, n, m) && grid[nx][ny]==1)
 48                    {
 49                        grid[nx][ny]=2;
 50                        q.add(new int[]{nx,ny});
-51                        flag=true;
-52                    }
-53                }
-54            }
-55
-56            // atleast 1 orange got rotten, update time
-57            if(flag)
-58                elapsedTime++;
-59        }
-60
-61        // check if any fresh orange still available
-62        for(int i=0;i<n;i++){
-63            for(int j=0;j<m;j++){
-64                if(grid[i][j]==1){
-65                    return -1;
-66                }
-67            }
-68        }
-69
-70        return elapsedTime;
-71    }
-72}
+51                        freshOrange--;
+52                        flag=true;
+53                    }
+54                }
+55            }
+56
+57            // atleast 1 orange got rotten, update time
+58            if(flag)
+59                elapsedTime++;
+60        }
+61
+62        // // check if any fresh orange still available
+63        // for(int i=0;i<n;i++){
+64        //     for(int j=0;j<m;j++){
+65        //         if(grid[i][j]==1){
+66        //             return -1;
+67        //         }
+68        //     }
+69        // }
+70
+71        return freshOrange==0? elapsedTime: -1;
+72    }
+73}
