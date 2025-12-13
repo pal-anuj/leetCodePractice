fdea0fb4c91481b95cf414204d8baf86d478f00a
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2025-12-14 00:31:26 +0530

    File updated at 14/12/2025, 00:31:26

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,20 +1,20 @@
-// Last updated: 29/08/2025, 23:54:25
-class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int n=nums.length;
-        int[] leftPr= new int[n];
-        leftPr[0]=1;
-        for(int i=1;i<n;i++){
-            leftPr[i]=leftPr[i-1]*nums[i-1];
-        }
-        
-        int[] res= new int[n];
-        int suffix=1;
-        for(int i=n-1;i>=0;i--){
-            res[i]= leftPr[i]*suffix;
-            suffix= suffix*nums[i];
-        }
-        return res;
-    }
-
-}
+// Last updated: 14/12/2025, 00:31:26
+1class Solution {
+2    public int[] productExceptSelf(int[] nums) {
+3
+4       int n=nums.length; 
+5       int[] lPro= new int[n];
+6       lPro[0]=1;
+7       for(int i=1;i<n;i++){
+8            lPro[i]= lPro[i-1]*nums[i-1];
+9       }
+10
+11       int[] res= new int[n];
+12       int suffix=1;
+13       for(int i=n-1;i>=0;i--){
+14            res[i]= lPro[i]*suffix;
+15            suffix= suffix*nums[i];
+16       }
+17       return res;
+18    }
+19}
