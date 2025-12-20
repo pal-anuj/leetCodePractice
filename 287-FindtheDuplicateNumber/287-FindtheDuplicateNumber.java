2d02406973fd7a377cced2d498fbd73956c5c413
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2025-12-20 20:02:25 +0530

    File updated at 20/12/2025, 20:02:24

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
@@ -1,14 +1,14 @@
-// Last updated: 20/12/2025, 19:35:00
-1class Solution {
-2    public int findDuplicate(int[] arr) {
-3        //using extra space
-4        HashSet<Integer> hs= new HashSet<>();
-5        for(int i=0;i<arr.length;i++){
-6            if(hs.contains(arr[i]))
-7                return arr[i];
-8            else
-9                hs.add(arr[i]);
-10        }
-11        return 0;
-12    }
-13}
+// Last updated: 20/12/2025, 20:02:24
+class Solution {
+    public int findDuplicate(int[] nums) {
+        boolean[] flag=new boolean[nums.length];
+        for(int i=0;i<nums.length;i++){
+            int n=nums[i];
+            if(flag[n]){
+                return n;
+            }
+            flag[n]=true;
+        }
+        return -1;
+    }
+}
