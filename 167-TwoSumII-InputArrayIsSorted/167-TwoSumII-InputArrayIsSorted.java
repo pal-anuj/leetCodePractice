b5c96545e698d1c3142239f9560a58fad106b103
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2025-12-20 19:23:33 +0530

    File updated at 20/12/2025, 19:23:32

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -1,22 +1,20 @@
-// Last updated: 20/12/2025, 19:23:13
+// Last updated: 20/12/2025, 19:23:32
 1class Solution {
 2    public int[] twoSum(int[] arr, int target) {
 3        int n= arr.length;
-4        int min= 0;
-5        int i;
-6        int low=0;
-7        int high=n-1;
-8        while(low<high){
-9            int sum= arr[low]+arr[high];
-10            if(sum==target)
-11                break;   
-12            else if(sum > target){
-13                high--;
-14            }
-15            else
-16                low++;
-17        }
-18        int[] res= { low+1, high+1};
-19        return res;
-20    }
-21}
+4        int low=0;
+5        int high=n-1;
+6        while(low<high){
+7            int sum= arr[low]+arr[high];
+8            if(sum==target)
+9                break;   
+10            else if(sum > target){
+11                high--;
+12            }
+13            else
+14                low++;
+15        }
+16        int[] res= { low+1, high+1};
+17        return res;
+18    }
+19}
