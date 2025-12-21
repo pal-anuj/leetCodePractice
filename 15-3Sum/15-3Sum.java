358e96276de023e82110169f9ace6af11dbd6d04
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2025-12-21 12:46:41 +0530

    File updated at 21/12/2025, 12:46:40

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,4 +1,4 @@
-// Last updated: 21/12/2025, 12:39:04
+// Last updated: 21/12/2025, 12:46:40
 1class Solution {
 2    public List<List<Integer>> threeSum(int[] arr) {
 3        
@@ -7,34 +7,31 @@
 6        
 7        /* brute force approach Start*/
 8        Set<List<Integer>> set= new HashSet<>();
-9        Map<Integer,Integer> map= new HashMap<>();
+9
 10        for(int i=0;i<n;i++){
-11            map.put(arr[i],i);
-12        }
-13
-14        for(int i=0;i<n;i++){
-15            int x= arr[i];
-16            for(int j=i+1;j<n;j++){
-17                int y=arr[j];
-18
-19                int z= -1 * (x+y);
-20                if(map.containsKey(z))
-21                {
-22                    int k=map.get(z);
-23                    if(i!=j && j!=k && k!=i ){
-24                        List<Integer> ls= new ArrayList<>(Arrays.asList(x, y, z));
-25                        Collections.sort(ls);
-26                        if(!set.contains(ls))
-27                        {
-28                            res.add(ls);
-29                            set.add(ls);
-30                        }    
-31                    }
-32                }
-33            }
-34        }
-35        /* brute force approach end*/
-36
-37        return res;
-38    }
-39}
+11            int x= arr[i];
+12            HashSet<Integer> hs= new HashSet<>();
+13            for(int j=i+1;j<n;j++){
+14                int y=arr[j];
+15
+16                int z= -1 * (x+y);
+17                if(hs.contains(z))
+18                {
+19                    if(x+y+z==0 ){
+20                        List<Integer> ls= new ArrayList<>(Arrays.asList(x, y, z));
+21                        Collections.sort(ls);
+22                        if(!set.contains(ls))
+23                        {
+24                            res.add(ls);
+25                            set.add(ls);
+26                        }    
+27                    }
+28                }
+29                hs.add(y);
+30            }
+31        }
+32        /* brute force approach end*/
+33
+34        return res;
+35    }
+36}
