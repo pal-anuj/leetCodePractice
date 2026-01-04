5f5f7df67c747ee9362ae3b0a3eb7e924a81f120
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-01-04 12:03:02 +0530

    File updated at 04/01/2026, 12:03:02

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,4 +1,4 @@
-// Last updated: 04/01/2026, 11:58:45
+// Last updated: 04/01/2026, 12:03:02
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -12,66 +12,26 @@
 11class Solution {
 12    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 13        
-14        ListNode i1= l1;
-15        ListNode i2= l2;
-16
-17        int carry=0;
-18        ListNode res=new ListNode(0);
-19        ListNode curr= res;
-20        while(i1 != null && i2 != null){
-21            int num = i1.val + i2.val + carry;
-22            if(num>9){
-23                carry= num / 10; 
-24                num= num % 10;
-25            }
-26            else
-27                carry=0;
-28            // System.out.println(num);
+14        ListNode res = new ListNode(0);
+15        ListNode tail = res;
+16        int carry = 0;
+17
+18        while (l1 != null || l2 != null || carry != 0) {
+19            int digit1 = (l1 != null) ? l1.val : 0;
+20            int digit2 = (l2 != null) ? l2.val : 0;
+21
+22            int sum = digit1 + digit2 + carry;
+23            int digit = sum % 10;
+24            carry = sum / 10;
+25
+26            ListNode newNode = new ListNode(digit);
+27            tail.next = newNode;
+28            tail = tail.next;
 29
-30            curr.next= new ListNode(num);
-31            curr= curr.next;
-32
-33            i1= i1.next;
-34            i2= i2.next;
-35        }
-36
-37        if(i1 != null){
-38
-39            while(i1 != null){
-40                int num = i1.val + carry;
-41                if(num>9){
-42                    carry= num / 10; 
-43                    num= num % 10;
-44                }
-45                else
-46                    carry=0;
-47                curr.next= new ListNode(num);
-48                curr= curr.next;
-49                i1= i1.next;
-50            }
-51        }
-52        else if(i2 != null){
-53
-54            while(i2 != null){
-55                int num = i2.val + carry;
-56                if(num>9){
-57                    carry= num / 10; 
-58                    num= num % 10;
-59                }
-60                else
-61                    carry=0;
-62                curr.next= new ListNode(num);
-63                curr= curr.next;
-64                i2= i2.next;
-65            }
-66        }
-67
-68        if(carry!=0)
-69         {
-70                curr.next= new ListNode(carry);
-71                curr= curr.next;
-72         }   
-73
-74        return res.next;
-75    }
-76}
+30            l1 = (l1 != null) ? l1.next : null;
+31            l2 = (l2 != null) ? l2.next : null;
+32        }
+33
+34        return res.next;
+35    }
+36}
