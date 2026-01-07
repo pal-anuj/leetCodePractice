595e32ad2d0db22e75b011862a82717e9bf0c4a0
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-01-07 09:29:34 +0530

    File updated at 07/01/2026, 09:29:34

--- 138-CopyListwithRandomPointer/138-CopyListwithRandomPointer.java
+++ 138-CopyListwithRandomPointer/138-CopyListwithRandomPointer.java
@@ -1,4 +1,4 @@
-// Last updated: 05/01/2026, 23:25:28
+// Last updated: 07/01/2026, 09:29:34
 1/*
 2// Definition for a Node.
 3class Node {
@@ -17,21 +17,52 @@
 16class Solution {
 17    public Node copyRandomList(Node head) {
 18        
-19        Node curr= head;
-20        Map<Node,Node> map= new HashMap<>();
-21        while(curr!=null){            
-22            map.put(curr, new Node(curr.val));
-23            curr= curr.next;
-24        }
-25
-26        for(Node temp=head;temp!=null; temp=temp.next)
-27        {
-28            Node cloneCurr= map.get(temp);
-29            cloneCurr.next= map.get(temp.next);
-30            cloneCurr.random= map.get(temp.random);
-31        }
-32
-33        Node newHead= map.get(head);
-34        return newHead;
-35    }
-36}
+19        if(head==null) return null;
+20        Node next,res;
+21        for(Node curr=head; curr!=null;){
+22            next= curr.next;
+23            curr.next= new Node(curr.val);
+24            curr.next.next= next;
+25            curr=next;
+26        }
+27
+28        for(Node curr=head; curr!=null;curr= curr.next.next){
+29            curr.next.random= (curr.random!=null)? curr.random.next: null;
+30        }
+31
+32        Node original= head; Node copy= head.next;
+33        res= copy;
+34        while(original!=null && copy!=null){
+35
+36            original.next= 
+37                original.next!=null? original.next.next: original.next;
+38            
+39            copy.next= 
+40                copy.next!=null? copy.next.next: copy.next;
+41            
+42            original= original.next;
+43            copy= copy.next;
+44        }
+45
+46        return res;
+47
+48        /*
+49        Node curr= head;
+50        Map<Node,Node> map= new HashMap<>();
+51        while(curr!=null){            
+52            map.put(curr, new Node(curr.val));
+53            curr= curr.next;
+54        }
+55
+56        for(Node temp=head;temp!=null; temp=temp.next)
+57        {
+58            Node cloneCurr= map.get(temp);
+59            cloneCurr.next= map.get(temp.next);
+60            cloneCurr.random= map.get(temp.random);
+61        }
+62
+63        Node newHead= map.get(head);
+64        return newHead;
+65        */
+66    }
+67}
