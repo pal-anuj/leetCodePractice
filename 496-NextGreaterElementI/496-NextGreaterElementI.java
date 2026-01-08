2573cd26c1029e122ca507509b537b8d7a84a6bd
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-01-09 01:06:27 +0530

    File updated at 09/01/2026, 01:06:27

--- 496-NextGreaterElementI/496-NextGreaterElementI.java
+++ 496-NextGreaterElementI/496-NextGreaterElementI.java
@@ -1,27 +1,29 @@
-// Last updated: 09/01/2026, 01:03:15
+// Last updated: 09/01/2026, 01:06:27
 1class Solution {
 2    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 3
-4        int n1= nums1.length;
-5        int n2= nums2.length;
-6        int[] res= new int[n1];  
-7        Map<Integer, Integer> nge= new HashMap<>();      
-8        Stack<Integer> st= new Stack<Integer>();
-9        
-10        st.push(nums2[n2-1]);
-11        nge.put(nums2[n2-1], -1);
-12        for(int i=n2-1; i>=0; i--){
-13            while(!st.isEmpty() && st.peek()<= nums2[i])
-14                st.pop();
-15
-16            int ng= st.isEmpty() ? -1: st.peek();
-17            nge.put(nums2[i], ng);   
-18            st.push(nums2[i]);
-19        }
-20
-21        for(int i=0;i<n1;i++){
-22            res[i]= nge.get(nums1[i]);
-23        }
-24        return res;
-25    }
-26}
+4        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+5        Stack<Integer> stack = new Stack<>();
+6
+7        // Process nums2
+8        for (int num : nums2) {
+9            while (!stack.isEmpty() && stack.peek() < num) {
+10                nextGreaterMap.put(stack.pop(), num);
+11            }
+12            stack.push(num);
+13        }
+14
+15        // Remaining elements have no next greater
+16        while (!stack.isEmpty()) {
+17            nextGreaterMap.put(stack.pop(), -1);
+18        }
+19
+20        // Build result for nums1
+21        int[] result = new int[nums1.length];
+22        for (int i = 0; i < nums1.length; i++) {
+23            result[i] = nextGreaterMap.get(nums1[i]);
+24        }
+25
+26        return result;
+27    }
+28}
