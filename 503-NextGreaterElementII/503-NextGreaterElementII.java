c0401bb699fbb2d29335ec426a739386655fa8fd
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-01-10 12:59:10 +0530

    File updated at 10/01/2026, 12:59:10

--- 503-NextGreaterElementII/503-NextGreaterElementII.java
+++ 503-NextGreaterElementII/503-NextGreaterElementII.java
@@ -1,19 +1,21 @@
-// Last updated: 10/01/2026, 12:48:36
-class Solution {
-    public int[] nextGreaterElements(int[] nums) {
-        Deque<Integer>st=new ArrayDeque<>();
-int n=nums.length;
-        int [] ans=new int[n];
-        Arrays.fill(ans,-1);
-        for(int i=0;i<2*n;i++){
-         int idx=i%n;
-         while(!st.isEmpty()&& nums[idx]>nums[st.peek()]){
-            ans[st.pop()]=nums[idx];
-         }
-         if(i<n){
-            st.push(idx);
-         }
-        }
-        return ans;
-    }
-}
+// Last updated: 10/01/2026, 12:59:10
+1class Solution {
+2    public int[] nextGreaterElements(int[] nums) {
+3        int n=nums.length;
+4        int[] res= new int[n];
+5        Stack<Integer> st= new Stack<>();
+6
+7        for(int i=2*n-1; i>=0 ; i--){
+8            int idx= i % n;
+9            while(!st.isEmpty() && st.peek() <= nums[idx])
+10                st.pop();
+11
+12            if(i<n){
+13                int nge= st.isEmpty() ? -1 : st.peek();
+14                res[idx]= nge;
+15            }
+16            st.push(nums[idx]);
+17        }
+18        return res;
+19    }
+20}
