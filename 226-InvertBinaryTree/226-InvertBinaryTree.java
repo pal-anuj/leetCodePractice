24805dbe77a84709791033bcb51f07b86e872739
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-01-15 11:24:57 +0530

    File updated at 15/01/2026, 11:24:57

--- 226-InvertBinaryTree/226-InvertBinaryTree.java
+++ 226-InvertBinaryTree/226-InvertBinaryTree.java
@@ -1,4 +1,4 @@
-// Last updated: 15/01/2026, 11:16:55
+// Last updated: 15/01/2026, 11:24:57
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -17,13 +17,33 @@
 16class Solution {
 17    public TreeNode invertTree(TreeNode root) {
 18        if(root==null)
-19            return null;            
-20        TreeNode temp= root.left;
-21        root.left= root.right;
-22        root.right= temp;
+19            return null;
+20
+21        Queue<TreeNode> q= new LinkedList<>();
+22        q.add(root);
 23
-24        invertTree(root.left);
-25        invertTree(root.right);
-26        return root;
-27    }
-28}
+24        while(!q.isEmpty()){
+25            TreeNode curr= q.poll();
+26
+27            // Shuffle left child with right child
+28            TreeNode temp= curr.left;
+29            curr.left= curr.right;
+30            curr.right= temp;
+31
+32            if(curr.left!=null) q.offer(curr.left);
+33            if(curr.right!=null) q.offer(curr.right);
+34        }
+35        return root;
+36        
+37        ////DFS use StackOverflow
+38        // if(root==null)
+39        //     return null;            
+40        // TreeNode temp= root.left;
+41        // root.left= root.right;
+42        // root.right= temp;
+43
+44        // invertTree(root.left);
+45        // invertTree(root.right);
+46        // return root;
+47    }
+48}
