aea1fdbd9c84f86fcebdbcea90d0208e1615a2b3
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-01-15 22:00:39 +0530

    File updated at 15/01/2026, 22:00:39

--- 110-BalancedBinaryTree/110-BalancedBinaryTree.java
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.java
@@ -1,39 +1,42 @@
-// Last updated: 15/01/2026, 21:20:52
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    // for binary tree balanace factor should be 1,0
-18    boolean isbal=true;
-19    public boolean isBalanced(TreeNode root) {
-20        if(root==null) return true;
-21        DFS(root);
-22        return isbal;
-23    }
-24
-25    public int DFS(TreeNode root){
-26        if(root==null) return 0;
-27        if(!isbal) return 0;
-28        int leftheight= DFS(root.left);
-29        int rightheight= DFS(root.right);
-30
-31        int balfactor= leftheight - rightheight;
-32        if(balfactor > 1 || balfactor < -1)
-33            isbal=false;
-34
-35        return 1+ Math.max(DFS(root.left), DFS(root.right));
-36    }
-37}
-38
+// Last updated: 15/01/2026, 22:00:39
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        int left = heightOfBT(root.left);
+        int right = heightOfBT(root.right);
+
+        int diff = Math.abs(left - right);
+        if(diff>1){
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int heightOfBT(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+
+        int left = heightOfBT(root.left);
+        int right = heightOfBT(root.right);
+
+        return Math.max(left,right)+1;
+    }
+}
