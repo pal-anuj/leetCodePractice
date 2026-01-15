f66b83a98fd6f7feb58a4a7cf7f283105019d29a
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-01-15 13:11:34 +0530

    File updated at 15/01/2026, 13:11:34

--- 101-SymmetricTree/101-SymmetricTree.java
+++ 101-SymmetricTree/101-SymmetricTree.java
@@ -1,4 +1,4 @@
-// Last updated: 15/01/2026, 12:47:13
+// Last updated: 15/01/2026, 13:11:34
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -16,20 +16,45 @@
 15 */
 16class Solution {
 17    public boolean isSymmetric(TreeNode root) {
-18        if (root == null)
-19            return false;
-20        return isSymmetricHelp(root.left, root.right);
-21    }
-22
-23    public boolean isSymmetricHelp(TreeNode left, TreeNode right) {
-24        if (left == null || right == null)
-25            return left == right;
+18        if(root==null) return true;
+19
+20        //BFS
+21        Queue<TreeNode> q1= new LinkedList<>();
+22        Queue<TreeNode> q2= new LinkedList<>();
+23
+24        q1.offer(root.left);
+25        q2.offer(root.right);
 26
-27        if(left.val!= right.val)
-28            return false;
-29
-30        return  isSymmetricHelp(left.left, right.right) 
-31                && 
-32                isSymmetricHelp(left.right, right.left);     
-33    }
-34}
+27        while(!q1.isEmpty() && !q2.isEmpty()){
+28            TreeNode c1= q1.poll();
+29            TreeNode c2= q2.poll();
+30
+31            if(c1==null && c2==null) continue;
+32            if(c1==null || c2==null) return false;
+33            if(c1.val != c2.val) return false;
+34
+35            q1.offer(c1.left);    
+36            q1.offer(c1.right);    
+37            q2.offer(c2.right);
+38            q2.offer(c2.left);
+39        }
+40        return true;
+41        
+42        //// DFS
+43        // if (root == null)
+44        //     return false;
+45        // return isSymmetricHelp(root.left, root.right);
+46    }
+47
+48    public boolean isSymmetricHelp(TreeNode left, TreeNode right) {
+49        if (left == null || right == null)
+50            return left == right;
+51
+52        if(left.val!= right.val)
+53            return false;
+54
+55        return  isSymmetricHelp(left.left, right.right) 
+56                && 
+57                isSymmetricHelp(left.right, right.left);     
+58    }
+59}
