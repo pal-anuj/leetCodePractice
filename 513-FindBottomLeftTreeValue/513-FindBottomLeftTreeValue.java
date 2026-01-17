e19df356a69291b5009d988e7ca3624873ee3391
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-01-17 12:28:08 +0530

    File updated at 17/01/2026, 12:28:08

--- 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.java
+++ 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.java
@@ -1,40 +1,35 @@
-// Last updated: 17/01/2026, 12:24:01
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17
-18    public int findBottomLeftValue(TreeNode root) {
-19        
-20        int bottomLeftestEle= Integer.MIN_VALUE;            
-21        Queue<TreeNode> q= new LinkedList<>();
-22        q.add(root);
-23        
-24        while(!q.isEmpty()){
-25            int n=q.size();
-26            for(int i=0;i< n;i++){
-27                TreeNode curr= q.poll();            
-28                if(i==0){
-29                    bottomLeftestEle= curr.val;
-30                    System.out.println("i="+i+" , currVal= "+curr.val );
-31                }
-32                
-33                if(curr.left!=null) q.add(curr.left);
-34                if(curr.right!=null) q.add(curr.right);
-35            }
-36        }
-37        return bottomLeftestEle;
-38    }
-39}
+// Last updated: 17/01/2026, 12:28:08
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            root = q.poll();
+
+            if(root.right != null) {
+                q.add(root.right);
+            }
+
+            if(root.left != null) {
+                q.add(root.left);
+            }
+        }
+
+        return root.val;
+    }
+}
