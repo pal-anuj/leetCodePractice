685f1fee742270762dbaa33c143f28f1513975f0
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-01-22 20:06:01 +0530

    File updated at 22/01/2026, 20:06:01

--- 88-MergeSortedArray/88-MergeSortedArray.java
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -1,24 +1,17 @@
-// Last updated: 22/01/2026, 09:35:16
+// Last updated: 22/01/2026, 20:06:01
 1class Solution {
 2    public void merge(int[] nums1, int m, int[] nums2, int n) {
 3
-4        int i=m-1;
-5        int j=n-1;
-6        int k=m+n-1;
+4        int i = m - 1;
+5        int j = n - 1;
+6        int k = m + n - 1;
 7
-8        while(j>=0){
-9            // if nums1 is exhausted OR nums2[j] is bigger
-10            if (i < 0 || nums2[j] > nums1[i]) {
-11                nums1[k] = nums2[j];
-12                j--;
-13            } else {
-14                nums1[k] = nums1[i];
-15                i--;
-16            }
-17            k--;
-18        }
-19    }
-20}
-21
-22//12358->1234
-23//467->567
+8        while (j >= 0) {
+9            if (i < 0 || nums2[j] > nums1[i]) {
+10                nums1[k--] = nums2[j--];
+11            } else {
+12                nums1[k--] = nums1[i--];
+13            }
+14        }
+15    }
+16}
