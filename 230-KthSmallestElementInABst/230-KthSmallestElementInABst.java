d29f2aa956a8511da1bc2ddbe58ea71c44f6f004
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-01-24 00:10:01 +0530

    Create 230-KthSmallestElementInABst

---
+++ 230-KthSmallestElementInABst/230-KthSmallestElementInABst.java
@@ -0,0 +1,42 @@
+// Last updated: 24/01/2026, 00:10:01
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int prev = 0;
+
+    public int kthSmallest(TreeNode root, int k) {
+        if (root == null)
+            return -1;
+
+        if (root.left != null) {
+            int leftAns = kthSmallest(root.left, k);
+            if (leftAns != -1)
+                return leftAns;
+        }
+
+        prev++;
+        if (prev == k)
+            return root.val;
+
+        if (root.right != null) {
+            int rightAns = kthSmallest(root.right, k);
+            if (rightAns != -1)
+                return rightAns;
+        }
+        return -1;
+    }
+}
