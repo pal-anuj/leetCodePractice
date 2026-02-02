7d74d616ad91e9cb78386183b4c301e15f1f6ae0
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-02 22:18:47 +0530

    File updated at 02/02/2026, 22:18:47

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,4 +1,4 @@
-// Last updated: 02/02/2026, 22:18:34
+// Last updated: 02/02/2026, 22:18:47
 1class Solution {
 2
 3    public int coinChange(int[] coins, int amount) {
@@ -8,29 +8,29 @@
 7        for (int i = 0; i <= n; i++) {
 8            for (int j = 0; j <= amount; j++) {
 9                if (i == 0)
-10                    t[i][j] = Integer.MAX_VALUE-1;
+10                    t[i][j] = Integer.MAX_VALUE - 1;
 11                if (j == 0)
-12                    t[i][j]= 0;
+12                    t[i][j] = 0;
 13            }
 14        }
 15
-16        int i=1;    
+16        int i = 1;
 17        for (int j = 1; j <= amount; j++) {
-18            if(j % coins[0] == 0)
-19                t[i][j] = j/coins[0];
+18            if (j % coins[0] == 0)
+19                t[i][j] = j / coins[0];
 20            else
-21                t[i][j]= Integer.MAX_VALUE-1;    
+21                t[i][j] = Integer.MAX_VALUE - 1;
 22        }
 23
-24        for ( i = 2; i <= n; i++) {
+24        for (i = 2; i <= n; i++) {
 25            for (int j = 1; j <= amount; j++) {
 26                if (coins[i - 1] <= j) {
-27                    t[i][j] = Math.min( 1 + t[i][j - coins[i - 1]]  , t[i - 1][j]);
+27                    t[i][j] = Math.min(1 + t[i][j - coins[i - 1]], t[i - 1][j]);
 28                } else
 29                    t[i][j] = t[i - 1][j];
 30            }
 31        }
 32
-33        return t[n][amount]==Integer.MAX_VALUE - 1 ? -1 : t[n][amount];
+33        return t[n][amount] == Integer.MAX_VALUE - 1 ? -1 : t[n][amount];
 34    }
 35}
