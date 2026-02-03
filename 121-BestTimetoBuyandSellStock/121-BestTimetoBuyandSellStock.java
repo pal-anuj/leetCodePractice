75c62f5858a16ba5d95c5e84d36c51399273d690
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-03 07:31:11 +0530

    File updated at 03/02/2026, 07:31:11

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,30 +1,14 @@
-// Last updated: 16/12/2025, 09:06:43
-class Solution {
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-    }
-
-    public int maxProfit(int[] prices) {
-        int profit = 0;
-        int maxProfit = 0;
-        int st = 0;
-        int end = 1;
-
-        while (end < prices.length) {
-            profit = prices[end] - prices[st];
-            maxProfit = Math.max(maxProfit, profit);
-
-            if (profit < 0) {
-                st = end;
-            }
-            end++;
-        }
-
-        return maxProfit;
-    }
-}
+// Last updated: 03/02/2026, 07:31:11
+1class Solution {
+2    public int maxProfit(int[] prices) {
+3        int profit=0;
+4        int minPrice= prices[0];
+5        for(int i=1;i<prices.length;i++){
+6            if(prices[i] > minPrice)
+7                profit= Math.max(profit, prices[i] - minPrice);
+8            else
+9                minPrice=prices[i];    
+10        }
+11        return profit;
+12    }
+13}
