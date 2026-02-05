a0e47942b5fad94eae154bde7b2341c6397a5b8e
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-05 23:19:10 +0530

    File updated at 05/02/2026, 23:19:10

--- 58-LengthofLastWord/58-LengthofLastWord.java
+++ 58-LengthofLastWord/58-LengthofLastWord.java
@@ -1,8 +1,22 @@
-// Last updated: 05/02/2026, 23:11:49
-1class Solution {
-2    public int lengthOfLastWord(String s) {
-3        List<String> strList= Arrays.asList(s.split(" "));
-4        int n= strList.size()-1;
-5        return strList.get(n).length();
-6    }
-7}
+// Last updated: 05/02/2026, 23:19:10
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int a=0;
+        int length=s.length()-1;
+
+        while(s.charAt(length)==' ')
+        {
+            length=length-1;
+        }
+        for(int i=length; i>=0; i--){
+
+            if(s.charAt(i)!=' '){
+                a++;
+            }
+            else{
+                break;
+            }
+        }
+        return a;
+    }
+}
