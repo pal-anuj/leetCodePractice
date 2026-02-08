d9f73e39abe85ced9bb614761f5fb0100885e812
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-08 15:41:51 +0530

    File updated at 08/02/2026, 15:41:51

--- 3834-MergeAdjacentEqualElements/3834-MergeAdjacentEqualElements.java
+++ 3834-MergeAdjacentEqualElements/3834-MergeAdjacentEqualElements.java
@@ -1,32 +1,19 @@
-// Last updated: 08/02/2026, 15:26:54
-1class Solution {
-2    //[4,1,1,2]
-3    public List<Long> mergeAdjacent(int[] nums) {
-4
-5        Stack<Long> st = new Stack<>();
-6        for (int i = 0; i < nums.length; i++) {
-7
-8            st.push((long) nums[i]);
-9            while (st.size() >= 2) {
-10                long top1 = st.pop();
-11                long top2 = st.pop();
-12
-13                if (top1 == top2) {
-14                    st.push(top1 + top2);
-15                } else {
-16                    st.push(top2);
-17                    st.push(top1);
-18                    break;
-19                }
-20            }
-21        }
-22
-23        List<Long> res = new ArrayList<>();
-24        while (!st.isEmpty()) {
-25            res.add(st.pop());
-26        }
-27
-28        Collections.reverse(res);
-29        return res;
-30    }
-31}
+// Last updated: 08/02/2026, 15:41:51
+class Solution {
+    public List<Long> mergeAdjacent(int[] nums) {
+        Stack<Long> st = new Stack<>();
+        for(int ele: nums){
+            long num = ele;
+            while(!st.isEmpty() && st.peek() == num){
+                st.pop();
+                num += num;
+            }
+            st.push(num);
+        }
+        List<Long> ans = new ArrayList<>();
+        for(int i=0; i<st.size(); i++){
+            ans.add(st.get(i));
+        }
+        return ans;
+    }
+}
