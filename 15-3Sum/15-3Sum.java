206c95618fd698f19acb3d74629dc6d522ae80b1
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-11 06:00:48 +0530

    File updated at 11/02/2026, 06:00:48

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,37 +1,33 @@
-// Last updated: 16/01/2026, 22:50:59
+// Last updated: 11/02/2026, 06:00:48
 1class Solution {
-2    public List<List<Integer>> threeSum(int[] arr) {
-3        
-4        int n=arr.length;
-5        List<List<Integer>> res= new ArrayList<>();
-6        
-7        /* brute force approach Start*/
-8        Set<List<Integer>> set= new HashSet<>();
-9
-10        for(int i=0;i<n;i++){
-11            int x= arr[i];
-12            HashSet<Integer> hs= new HashSet<>();
-13            for(int j=i+1;j<n;j++){
-14                int y=arr[j];
-15
-16                int z= -1 * (x+y);
-17                if(hs.contains(z))
-18                {
-19                    if(x+y+z==0 ){
-20                        List<Integer> ls= new ArrayList<>(Arrays.asList(x, y, z));
-21                        Collections.sort(ls);
-22                        if(!set.contains(ls))
-23                        {
-24                            res.add(ls);
-25                            set.add(ls);
-26                        }    
-27                    }
-28                }
-29                hs.add(y);
-30            }
-31        }
-32        /* brute force approach end*/
-33
-34        return res;
-35    }
-36}
+2    public List<List<Integer>> threeSum(int[] nums) {
+3        int n = nums.length;
+4        List<List<Integer>> res = new ArrayList<>();
+5        Set<List<Integer>> set = new HashSet<>();
+6
+7        Arrays.sort(nums);
+8        for (int i = 0; i < n; i++) {
+9            int diff = 0 - nums[i];
+10            int l = i + 1;
+11            int r = n - 1;
+12            while (l < r) {
+13                if (nums[l] + nums[r] > diff) {
+14                    r--;
+15                } else if (nums[l] + nums[r] < diff) {
+16                    l++;
+17                } else {
+18                    ArrayList<Integer> ls = 
+19                                new ArrayList<>(Arrays.asList(nums[i], nums[l], nums[r]));
+20
+21                    Collections.sort(ls);
+22                    if (!set.contains(ls)) {
+23                        set.add(ls);
+24                        res.add(ls);
+25                    }
+26                    l++;
+27                }
+28            }
+29        }
+30        return res;
+31    }
+32}
