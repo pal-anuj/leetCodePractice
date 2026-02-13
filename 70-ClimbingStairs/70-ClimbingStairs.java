755dc57f7dd627f45a6f07893ceacb639050c067
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-13 07:48:59 +0530

    File updated at 13/02/2026, 07:48:59

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,19 +1,18 @@
-// Last updated: 07/01/2026, 00:12:18
-1class Solution {
-2    public int climbStairs(int n) {
-3        if(n <= 0) return 0;
-4        if(n == 1) return 1;
-5
-6        int prev2= 1; // ways to reach step 0
-7        int prev1= 1; // ways to reach step 1
-8        int curr=0;
-9
-10        for(int i=2; i <= n; i++){
-11            curr= prev1 + prev2;
-12            prev2= prev1;
-13            prev1= curr;
-14        }
-15        return curr;
-16
-17    }
-18}
+// Last updated: 13/02/2026, 07:48:59
+1
+2class Solution {
+3    public int climbStairs(int n) {
+4
+5        if (n == 1)
+6            return n;
+7        int t[] = new int[n + 1];
+8        t[1] = 1;
+9        t[2] = 2;
+10
+11        for (int i = 3; i <= n; i++) {
+12            t[i] = t[i - 1] + t[i - 2];
+13        }
+14        return t[n];
+15    }
+16}
+17
