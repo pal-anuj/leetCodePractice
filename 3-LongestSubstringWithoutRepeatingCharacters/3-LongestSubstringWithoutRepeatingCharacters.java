4521a4ea810d057f055c71bd6d64bd1dfe3ef654
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-16 08:55:54 +0530

    File updated at 16/02/2026, 08:55:54

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,25 +1,17 @@
-// Last updated: 19/08/2025, 00:22:52
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        
-        Set<Character> set= new HashSet<>();
-        int right=0;
-        int left=0;
-        int maxLen=0;
-        while(right < s.length()){
-            char c= s.charAt(right);
-
-            while(set.contains(c)){
-                set.remove(s.charAt(left));
-                left++;
-            }
-
-            set.add(c);
-            maxLen= Math.max(maxLen, right-left+1);
-            right++;
-
-        }
-        return maxLen;
-
-    }
-}
+// Last updated: 16/02/2026, 08:55:54
+1class Solution {
+2    public double findMaxAverage(int[] nums, int k) {
+3        int sum = 0;
+4        for (int i = 0; i < k; i++) {
+5            sum += nums[i];
+6        }
+7
+8        int maxSum = sum;
+9        for (int i = k; i < nums.length; i++) {
+10            sum = sum + nums[i] - nums[i-k];
+11            maxSum = Math.max(maxSum, sum);
+12        }
+13
+14        return (double) maxSum / k;
+15    }
+16}
