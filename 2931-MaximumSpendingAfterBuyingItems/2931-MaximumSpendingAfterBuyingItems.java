4130fb57d079d5b3328ce6f3825fb55607a71ed0
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-17 05:50:23 +0530

    Create 2931-MaximumSpendingAfterBuyingItems

---
+++ 2931-MaximumSpendingAfterBuyingItems/2931-MaximumSpendingAfterBuyingItems.java
@@ -0,0 +1,21 @@
+// Last updated: 17/02/2026, 05:50:23
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+         Map<Character,Integer> map = new HashMap<>();
+        int left = 0;
+        int res = 0;
+
+        for(int right = 0; right < s.length(); right++){
+            char c = s.charAt(right);
+
+            if(map.containsKey(c)){
+                left = Math.max(left, map.get(c) + 1);
+            }
+
+            map.put(c, right);
+            res = Math.max(res, right - left + 1);
+        }
+
+        return res;
+    }
+}
