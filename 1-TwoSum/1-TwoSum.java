282b5d389686946aea697f381d9837974484f998
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-20 20:23:51 +0530

    File updated at 20/02/2026, 20:23:50

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,20 +1,17 @@
-// Last updated: 18/02/2026, 07:46:14
-1class NumArray {
-2
-3    int[] prefix;
-4    public NumArray(int[] nums) {
-5        prefix= new int[nums.length+1];
-6        for(int i=0;i<nums.length;i++)
-7            prefix[i+1]= prefix[i] + nums[i]; 
-8    }
-9    
-10    public int sumRange(int left, int right) {
-11        return prefix[right+1] - prefix[left];
-12    }
-13}
+// Last updated: 20/02/2026, 20:23:50
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        int[] res= new int[2];
+4        for(int i=0;i<nums.length;i++){
+5            for(int j=i+1;j<nums.length;j++){
+6                if(nums[i] + nums[j]==target){
+7                    res[0]= i;
+8                    res[1]= j;
+9                    break;
+10                }    
+11            }
+12        }
+13        return res;
 14
-15/**
-16 * Your NumArray object will be instantiated and called as such:
-17 * NumArray obj = new NumArray(nums);
-18 * int param_1 = obj.sumRange(left,right);
-19 */
+15    }
+16}
