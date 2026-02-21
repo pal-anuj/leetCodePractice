7574155c84f2782d80edecbb075037115cb75808
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-22 00:07:29 +0530

    File updated at 22/02/2026, 00:07:29

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
@@ -1,34 +1,50 @@
-// Last updated: 22/02/2026, 00:03:35
+// Last updated: 22/02/2026, 00:07:29
 1class Solution {
 2    public int firstUniqChar(String s) {
 3        int n = s.length();
 4
-5        Map<Character, Integer> map = new HashMap<>();
-6
-7        for (int i = 0; i < n; i++) {
-8            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-9        }
-10
-11        for (int i = 0; i < n; i++) {
-12            if (map.get(s.charAt(i)) == 1)
-13                return i;
-14        }
-15        return -1;
-16
-17        /* // Brute force approach
-18        for (int i = 0; i < n; i++) {
-19            boolean repeated = false;
-20            for (int j = 0; j < n; j++) {
-21                if (s.charAt(i) == s.charAt(j) && i != j) {
-22                    repeated = true;
-23                    break;
-24                }
-25            }
-26            if (repeated == false)
-27                return i;
-28        }
-29        return -1;
-30        */
-31
-32    }
-33}
+5        // optimized Approach
+6        int[] arr = new int[26];
+7        Arrays.fill(arr, 0);
+8        for (int i = 0; i < n; i++) {
+9            arr[s.charAt(i) - 'a']++;
+10        }
+11
+12        for (int i = 0; i < n; i++) {
+13            if (arr[s.charAt(i) - 'a'] == 1)
+14                return i;
+15        }
+16        return -1;
+17
+18        /*
+19        // Efficient Approach
+20        Map<Character, Integer> map = new HashMap<>();
+21        
+22        for (int i = 0; i < n; i++) {
+23            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+24        }
+25        
+26        for (int i = 0; i < n; i++) {
+27            if (map.get(s.charAt(i)) == 1)
+28                return i;
+29        }
+30        return -1;
+31        */
+32
+33        /* // Brute force approach
+34        for (int i = 0; i < n; i++) {
+35            boolean repeated = false;
+36            for (int j = 0; j < n; j++) {
+37                if (s.charAt(i) == s.charAt(j) && i != j) {
+38                    repeated = true;
+39                    break;
+40                }
+41            }
+42            if (repeated == false)
+43                return i;
+44        }
+45        return -1;
+46        */
+47
+48    }
+49}
