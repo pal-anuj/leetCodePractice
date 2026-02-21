45bf05275e522ac24a42c3141919898efe951bb5
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-22 00:03:35 +0530

    File updated at 22/02/2026, 00:03:35

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
@@ -1,18 +1,34 @@
-// Last updated: 21/02/2026, 23:58:58
+// Last updated: 22/02/2026, 00:03:35
 1class Solution {
 2    public int firstUniqChar(String s) {
 3        int n = s.length();
-4        for (int i = 0; i < n; i++) {
-5            boolean repeated = false;
-6            for (int j = 0; j < n; j++) {
-7                if (s.charAt(i) == s.charAt(j) && i != j) {
-8                    repeated = true;
-9                    break;
-10                }
-11            }
-12            if (repeated == false)
+4
+5        Map<Character, Integer> map = new HashMap<>();
+6
+7        for (int i = 0; i < n; i++) {
+8            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+9        }
+10
+11        for (int i = 0; i < n; i++) {
+12            if (map.get(s.charAt(i)) == 1)
 13                return i;
 14        }
 15        return -1;
-16    }
-17}
+16
+17        /* // Brute force approach
+18        for (int i = 0; i < n; i++) {
+19            boolean repeated = false;
+20            for (int j = 0; j < n; j++) {
+21                if (s.charAt(i) == s.charAt(j) && i != j) {
+22                    repeated = true;
+23                    break;
+24                }
+25            }
+26            if (repeated == false)
+27                return i;
+28        }
+29        return -1;
+30        */
+31
+32    }
+33}
