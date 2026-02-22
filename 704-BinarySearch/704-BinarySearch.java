871c4748e5c1341d0d5747cb470f26d2e80c9d61
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-22 08:25:23 +0530

    File updated at 22/02/2026, 08:25:23

--- 704-BinarySearch/704-BinarySearch.java
+++ 704-BinarySearch/704-BinarySearch.java
@@ -1,20 +1,29 @@
-// Last updated: 22/02/2026, 07:37:35
+// Last updated: 22/02/2026, 08:25:23
 1class Solution {
 2    public int search(int[] nums, int target) {
 3        int l = 0;
-4        int r = nums.length-1;
+4        int r = nums.length - 1;
 5
 6        while (l <= r) {
 7            int mid = l + (r - l) / 2;
 8
-9            if (nums[mid] > target) {
-10                r = mid - 1;
-11            } else if (nums[mid] < target) {
-12                l = mid + 1;
-13            } else
-14                return mid;
-15        }
-16
-17        return -1;
-18    }
-19}
+9            if (nums[mid] == target)
+10                return mid;
+11            else if (nums[l] <= nums[mid]) {
+12
+13                if (nums[mid] > target && nums[l] <= target) {
+14                    r = mid - 1;
+15                } else {
+16                    l = mid + 1;
+17                }
+18            } else {
+19                if (nums[mid] < target && nums[r] >= target)
+20                    l = mid + 1;
+21                else
+22                    r = mid - 1;
+23            }
+24        }
+25
+26        return -1;
+27    }
+28}
