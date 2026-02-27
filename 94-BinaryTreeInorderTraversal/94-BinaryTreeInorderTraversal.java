e1d75d3487b602a688368164063c325d45e8c8cf
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-02-27 08:30:01 +0530

    File updated at 27/02/2026, 08:30:01

--- 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.java
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.java
@@ -1,34 +1,28 @@
-// Last updated: 27/02/2026, 05:38:04
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public List<Integer> inorderTraversal(TreeNode root) {
+// Last updated: 27/02/2026, 08:30:01
+1class Solution {
+2    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+3
+4        List<List<Integer>> res = new ArrayList<>();
+5        backtrack(candidates, target, 0, new ArrayList<>(), res);
+6        return res;
+7    }
+8
+9    private void backtrack(int[] arr, int target, int start, List<Integer> curr, List<List<Integer>> res) {
+10
+11        if (target == 0) {
+12            res.add(new ArrayList<>(curr));
+13            return;
+14        }
+15
+16        if (target < 0)
+17            return;
 18
-19        List<Integer> res = new ArrayList<>();
-20        dfs(root, res);
-21        return res;
-22    }
-23
-24    private void dfs(TreeNode node, List<Integer> res) {
-25
-26        if (node == null)
-27            return;
-28
-29        dfs(node.left, res);
-30        res.add(node.val);
-31        dfs(node.right, res);
-32    }
-33}
+19        for (int i = start; i < arr.length; i++) {
+20            curr.add(arr[i]);
+21            backtrack(arr, target - arr[i], i, curr, res);
+22            curr.remove(curr.size() - 1);
+23        }
+24
+25    }
+26
+27}
