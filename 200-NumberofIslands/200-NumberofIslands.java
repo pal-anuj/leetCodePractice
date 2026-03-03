5444b8af89643f37eb1a60300b31f0053ed777b1
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-03-04 00:47:33 +0530

    File updated at 04/03/2026, 00:47:32

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,47 +1,47 @@
-// Last updated: 11/02/2026, 22:55:24
-class Solution {
-    private int[][] directions = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-	private boolean[][] visited;
-	private int row;
-	private int col;
-	private char[][] grid;
-    public int numIslands(char[][] grid) {
-        this.grid = grid;
-		this.row = grid.length;
-		this.col = grid[0].length;
-        visited = new boolean[row][col];
-		int ans = 0;
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				if (!visited[i][j] && grid[i][j] == '1') {
-					ans++;
-					bfs(i, j);
-				}
-			}
-		}
-		return ans;
-	}
-
-	private void bfs(int x, int y) {
-		Queue<int[]> queue = new LinkedList();
-		queue.add(new int[] { x, y });
-		visited[x][y] = true;
-
-		while (!queue.isEmpty()) {
-			int[] arr = queue.poll();
-			for (int[] dir : directions) {
-				int i = dir[0] + arr[0];
-				int j = dir[1] + arr[1];
-
-				if (i < 0 || i >= row || j < 0 || j >= col) {
-					continue;
-				}
-				if (visited[i][j] || grid[i][j] == '0') {
-					continue;
-				}
-				visited[i][j] = true;
-				queue.add(new int[] { i, j });
-			}
-		}
-    }
-}
+// Last updated: 04/03/2026, 00:47:32
+1class Solution {
+2    public static int[][] dir = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+3
+4    public int numIslands(char[][] grid) {
+5
+6        int n = grid.length;
+7        int m = grid[0].length;
+8        int count = 0;
+9
+10        boolean[][] vis = new boolean[n][m];
+11        for (int i = 0; i < n; i++) {
+12            for (int j = 0; j < m; j++) {
+13                if (grid[i][j] == '1' && !vis[i][j]) {
+14                    BFS(i, j, vis, grid);
+15                    count++;
+16                }
+17            }
+18        }
+19        return count;
+20    }
+21
+22    public void BFS(int i, int j, boolean[][] vis, char[][] grid) {
+23
+24        int n = grid.length;
+25        int m = grid[0].length;
+26        Queue<int[]> q = new LinkedList<>();
+27        q.add(new int[] { i, j });
+28        vis[i][j] = true;
+29
+30        while (!q.isEmpty()) {
+31
+32            int cell[] = q.poll();
+33            int row = cell[0];
+34            int col = cell[1];
+35            for (int[] d : dir) {
+36                int nr = row + d[0];
+37                int nc = col + d[1];
+38
+39                if (nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == '1' && !vis[nr][nc]) {
+40                    q.add(new int[] { nr, nc });
+41                    vis[nr][nc] = true;
+42                }
+43            }
+44        }
+45    }
+46}
