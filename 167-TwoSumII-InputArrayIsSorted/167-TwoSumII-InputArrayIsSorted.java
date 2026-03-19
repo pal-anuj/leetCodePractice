08096bff37345b78500d1b59c225fb5cbe25b55e
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-03-19 23:17:30 +0530

    File updated at 19/03/2026, 23:17:29

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -1,31 +1,16 @@
-// Last updated: 19/03/2026, 20:24:38
+// Last updated: 19/03/2026, 23:17:29
 1class Solution {
-2    public int removeDuplicates(int[] nums) {
+2    public void sortColors(int[] nums) {
 3
-4        //Efficient approach
-5        int l = 0;
-6        l++;
-7        for (int r = 1; r < nums.length; r++) {
-8            if (nums[r] != nums[r - 1]) {
-9                nums[l++] = nums[r];
-10            }
-11        }
-12        return l;
-13
-14        // // brute Force
-15        // if (nums.length == 0) return 0;
-16
-17        // List<Integer> ls = new ArrayList<>();
-18        // ls.add(nums[0]);
-19        // for (int i = 1; i < nums.length; i++) {
-20        //     if (nums[i] != nums[i - 1])
-21        //         ls.add(nums[i]);
-22        // }
-23
-24        // for (int i = 0; i < ls.size(); i++) {
-25        //     nums[i] = ls.get(i);
-26        // }
-27
-28        // return ls.size();
-29    }
-30}
+4        // Brute force approach, TimeComplexity O(n2) 
+5        for (int i = 0; i < nums.length; i++) {
+6            for (int j = 0; j < nums.length - 1; j++) {
+7                if (nums[j] > nums[j + 1]) {
+8                    int temp = nums[j];
+9                    nums[j] = nums[j + 1];
+10                    nums[j + 1] = temp;
+11                }
+12            }
+13        }
+14    }
+15}
