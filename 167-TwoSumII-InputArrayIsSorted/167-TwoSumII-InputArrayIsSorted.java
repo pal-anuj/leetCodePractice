11280d7763fa023b11cdbdd7e8a88a28368917d7
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-03-19 20:24:38 +0530

    File updated at 19/03/2026, 20:24:38

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -1,20 +1,31 @@
-// Last updated: 19/03/2026, 20:20:43
+// Last updated: 19/03/2026, 20:24:38
 1class Solution {
 2    public int removeDuplicates(int[] nums) {
-3        if (nums.length == 0)
-4            return 0;
-5
-6        List<Integer> ls = new ArrayList<>();
-7        ls.add(nums[0]);
-8        for (int i = 1; i < nums.length; i++) {
-9            if (nums[i] != nums[i - 1])
-10                ls.add(nums[i]);
+3
+4        //Efficient approach
+5        int l = 0;
+6        l++;
+7        for (int r = 1; r < nums.length; r++) {
+8            if (nums[r] != nums[r - 1]) {
+9                nums[l++] = nums[r];
+10            }
 11        }
-12
-13        for (int i = 0; i < ls.size(); i++) {
-14            nums[i] = ls.get(i);
-15        }
+12        return l;
+13
+14        // // brute Force
+15        // if (nums.length == 0) return 0;
 16
-17        return ls.size();
-18    }
-19}
+17        // List<Integer> ls = new ArrayList<>();
+18        // ls.add(nums[0]);
+19        // for (int i = 1; i < nums.length; i++) {
+20        //     if (nums[i] != nums[i - 1])
+21        //         ls.add(nums[i]);
+22        // }
+23
+24        // for (int i = 0; i < ls.size(); i++) {
+25        //     nums[i] = ls.get(i);
+26        // }
+27
+28        // return ls.size();
+29    }
+30}
