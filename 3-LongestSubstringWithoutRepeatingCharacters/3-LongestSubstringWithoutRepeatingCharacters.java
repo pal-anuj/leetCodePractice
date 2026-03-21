4e402fa9b01ffd40b4f5b88bfa718b3f1f5b92d4
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-03-21 22:03:51 +0530

    File updated at 21/03/2026, 22:03:50

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,17 +1,23 @@
-// Last updated: 16/02/2026, 08:55:54
+// Last updated: 21/03/2026, 22:03:50
 1class Solution {
-2    public double findMaxAverage(int[] nums, int k) {
-3        int sum = 0;
-4        for (int i = 0; i < k; i++) {
-5            sum += nums[i];
-6        }
-7
-8        int maxSum = sum;
-9        for (int i = k; i < nums.length; i++) {
-10            sum = sum + nums[i] - nums[i-k];
-11            maxSum = Math.max(maxSum, sum);
-12        }
-13
-14        return (double) maxSum / k;
-15    }
-16}
+2    public int lengthOfLongestSubstring(String s) {
+3        if (s.length() == 0)
+4            return 0;
+5        // if(s.length()==1) return 1;
+6        int maxL = 0;
+7        for (int i = 0; i < s.length(); i++) {
+8            int len = 1;
+9            maxL = Math.max(maxL, len);
+10            HashSet<Character> hs = new HashSet<>();
+11            hs.add(s.charAt(i));
+12            for (int j = i + 1; j < s.length(); j++) {
+13                if (hs.contains(s.charAt(j)))
+14                    break;
+15                else
+16                    maxL = Math.max(maxL, ++len);
+17                hs.add(s.charAt(j));
+18            }
+19        }
+20        return maxL;
+21    }
+22}
