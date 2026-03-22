edcbd7d07a791b3589f950096ecfe68687e18bdd
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-03-22 17:19:14 +0530

    File updated at 22/03/2026, 17:19:14

--- 1572-MatrixDiagonalSum/1572-MatrixDiagonalSum.java
+++ 1572-MatrixDiagonalSum/1572-MatrixDiagonalSum.java
@@ -1,34 +1,14 @@
-// Last updated: 22/03/2026, 17:13:54
+// Last updated: 22/03/2026, 17:19:14
 1class Solution {
-2    public int diagonalSum(int[][] mat) {
-3        int n = mat.length;
-4        int sum = 0;
-5
-6        // Efficient Approach
-7        for (int i = 0; i < n; i++) {
-8            // primary diagonal
-9            sum += mat[i][i];
-10
-11            // secondary diagonal
-12            sum += mat[i][n - 1 - i];
-13        }
-14
-15        if (n % 2 == 1)
-16            sum -= mat[n / 2][n / 2];
-17
-18        // // Brute Force    
-19        // for(int i=0;i<n;i++){
-20        //     for(int j=0;j<n;j++){
-21        //         // primary diagonal
-22        //         if(i==j)
-23        //             sum += mat[i][j];
-24
-25        //         // secondary diagonal
-26        //         else if(i+j == n-1)
-27        //             sum += mat[i][j];
-28        //     }
-29        // }
-30
-31        return sum;
-32    }
-33}
+2    public int[][] transpose(int[][] matrix) {
+3        int rows = matrix.length;
+4        int cols = matrix[0].length;
+5        int[][] res = new int[cols][rows];
+6        for (int i = 0; i < rows; i++) {
+7            for (int j = 0; j < cols; j++) {
+8                res[j][i] = matrix[i][j];
+9            }
+10        }
+11        return res;
+12    }
+13}
