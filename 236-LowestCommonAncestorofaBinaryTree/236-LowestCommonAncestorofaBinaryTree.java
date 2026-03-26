16caec55c6f91a0c54e59249adfc2c29898e615e
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-03-27 00:31:40 +0530

    File updated at 27/03/2026, 00:31:39

--- 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.java
+++ 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.java
@@ -1,32 +1,20 @@
-// Last updated: 27/03/2026, 00:24:40
+// Last updated: 27/03/2026, 00:31:39
 1class Solution {
-2    public int findDuplicate(int[] arr) {
-3        /* 
-4        //using extra space
-5        HashSet<Integer> hs= new HashSet<>();
-6        for(int i=0;i<arr.length;i++){
-7            if(hs.contains(arr[i]))
-8                return arr[i];
-9            else
-10                hs.add(arr[i]);
-11        }
-12        return 0;
-13        */
-14
-15        // fast slow pointer similar to cycle linkedlist 0(n) with space 0(1)
-16        int slow=arr[0];
-17        int fast=arr[0];
-18
-19        do{
-20            slow= arr[slow];
-21            fast= arr[arr[fast]];
-22        } while(slow != fast);
-23
-24        fast= arr[0];
-25        while(slow != fast){
-26            slow= arr[slow];
-27            fast= arr[fast];
-28        }
-29        return slow;
-30    }
-31}
+2    public int findDuplicate(int[] nums) {
+3        // [Head] -> (1) -> (3) -> (4) -> (2) -> (2) -> [NULL]
+4        int slow = nums[0];
+5        int fast = nums[0];
+6
+7        do {
+8            slow = nums[slow];
+9            fast = nums[nums[fast]];
+10        } while (slow != fast);
+11
+12        fast = nums[0];
+13        while (slow != fast) {
+14            slow = nums[slow];
+15            fast = nums[fast];
+16        }
+17        return slow;
+18    }
+19}
