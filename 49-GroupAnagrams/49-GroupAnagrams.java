e1b6bea187df7166bcc5140596bdf597fd1b2cc6
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-03-27 22:34:44 +0530

    File updated at 27/03/2026, 22:34:43

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,23 +1,23 @@
-// Last updated: 27/03/2026, 22:27:31
+// Last updated: 27/03/2026, 22:34:43
 1class Solution {
 2    public List<List<String>> groupAnagrams(String[] strs) {
-3        
-4        // Efficient Approach Time Complexity: O(n*klogk), Space complexity: O(n+k)
+3
+4        // Efficient Approach Time: O(n * k log k), Space: O(n * k)
 5        HashMap<String, List<String>> group = new HashMap<>();
-6        for (int i = 0; i < strs.length; i++) {
-7            char[] ch = strs[i].toCharArray();
-8            Arrays.sort(ch);
-9
-10            String key = new String(ch);
-11
-12            if (group.containsKey(key)) {
-13                List<String> l = group.get(key);
-14                l.add(strs[i]);
-15            } else {
-16                List<String> l = new ArrayList<>();
-17                l.add(strs[i]);
-18                group.put(key, l);
-19            }
+6        for (String str : strs) {
+7            char[] chars = str.toCharArray();
+8            Arrays.sort(chars); // sort characters
+9            String key = new String(chars);
+10
+11            group.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+12            // if (group.containsKey(key)) {
+13            //     List<String> l = group.get(key);
+14            //     l.add(strs[i]);
+15            // } else {
+16            //     List<String> l = new ArrayList<>();
+17            //     l.add(strs[i]);
+18            //     group.put(key, l);
+19            // }
 20        }
 21        return new ArrayList<>(group.values());
 22    }
