68cb26ec7e2c494fad45404fc1bbde77bbcc6176
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-04-01 09:06:01 +0530

    File updated at 01/04/2026, 09:06:00

--- 152-MaximumProductSubarray/152-MaximumProductSubarray.java
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.java
@@ -1,17 +1,22 @@
-// Last updated: 31/03/2026, 20:57:05
+// Last updated: 01/04/2026, 09:06:00
 1class Solution {
-2    public int[] dailyTemperatures(int[] temp) {
-3        int n = temp.length;
-4        int[] res = new int[n];
-5        Stack<Integer> st = new Stack<>();
-6        for (int i = 0; i < n; i++) {
-7
-8            while (!st.isEmpty() && temp[st.peek()] < temp[i]) {
-9                int idx = st.pop();
-10                res[idx] = i - idx;
-11            }
-12            st.push(i);
-13        }
-14        return res;
-15    }
-16}
+2    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+3        int[] res = new int[nums1.length];
+4        for (int i = 0; i < nums1.length; i++) {
+5            int nextgreater = -1;
+6            for (int j = 0; j < nums2.length; j++) {
+7                if (nums1[i] == nums2[j]) {
+8                    for (; j < nums2.length; j++) {
+9                        if (nums1[i] < nums2[j]) {
+10                            nextgreater = nums2[j];
+11                            break;
+12                        }
+13                    }
+14                    break;
+15                }
+16            }
+17            res[i] = nextgreater;
+18        }
+19        return res;
+20    }
+21}
