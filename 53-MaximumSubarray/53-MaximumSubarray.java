f02dab82a2be784583abc852f0b4abd3982f445f
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-04-12 05:19:35 +0530

    File updated at 12/04/2026, 05:19:34

--- 53-MaximumSubarray/53-MaximumSubarray.java
+++ 53-MaximumSubarray/53-MaximumSubarray.java
@@ -1,14 +1,14 @@
-// Last updated: 29/08/2025, 22:58:13
-class Solution {
-    public int maxSubArray(int[] arr) {
-
-        int maxSum=arr[0];
-        int currSum=arr[0];    
-        for(int i=1; i<arr.length;i++){
-            currSum= Math.max(arr[i], arr[i]+currSum);   
-            maxSum = Math.max(maxSum, currSum);
-        }
-        return maxSum;
-    }
-}
-
+// Last updated: 12/04/2026, 05:19:34
+1class Solution {
+2    public int maxSubArray(int[] nums) {
+3        int n = nums.length;
+4        int curr = nums[0];
+5        int maxSum = curr;
+6        for (int i = 1; i < n; i++) {
+7            curr += nums[i];
+8            curr = Math.max(curr, nums[i]);
+9            maxSum = Math.max(maxSum, curr);
+10        }
+11        return maxSum;
+12    }
+13}
