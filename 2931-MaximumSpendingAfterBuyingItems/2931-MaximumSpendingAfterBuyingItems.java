c42c9f70288adfac3df7787a37016e79e126106e
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-04-11 08:04:58 +0530

    File updated at 11/04/2026, 08:04:58

--- 2931-MaximumSpendingAfterBuyingItems/2931-MaximumSpendingAfterBuyingItems.java
+++ 2931-MaximumSpendingAfterBuyingItems/2931-MaximumSpendingAfterBuyingItems.java
@@ -1,17 +1,18 @@
-// Last updated: 11/04/2026, 08:01:15
+// Last updated: 11/04/2026, 08:04:58
 1class Solution {
 2    public int[] twoSum(int[] nums, int target) {
 3        int n = nums.length;
 4        int[] res = new int[2];
-5        for (int i = 0; i < n - 1; i++) {
-6            for (int j = i + 1; j < n; j++) {
-7                if (nums[i] + nums[j] == target) {
-8                    res[0] = i;
-9                    res[1] = j;
-10                    return res;
-11                }
+5        Map<Integer, Integer> map = new HashMap<>();
+6        for (int i = 0; i < n; i++) {
+7            int diff = target - nums[i];
+8            if (map.containsKey(diff)) {
+9                res[0] = map.get(diff);
+10                res[1] = i;
+11                break;
 12            }
-13        }
-14        return res;
-15    }
-16}
+13            map.put(nums[i], i);
+14        }
+15        return res;
+16    }
+17}
