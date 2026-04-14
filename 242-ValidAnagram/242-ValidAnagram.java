2f5d625d8a20a66d1141032d6264e308e84a7ea1
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-04-14 23:13:43 +0530

    File updated at 14/04/2026, 23:13:43

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,15 +1,17 @@
-// Last updated: 14/04/2026, 23:13:36
+// Last updated: 14/04/2026, 23:13:43
 1class Solution {
 2    public boolean isAnagram(String s, String t) {
-3        int[] freq= new int[26];
-4        if(s.length() != t.length()) return false;
-5        for(int i=0;i<s.length();i++){
-6            freq[s.charAt(i)-'a']++;
-7            freq[t.charAt(i)-'a']--;
-8        }
-9        for(int i=0;i<26;i++){
-10            if(freq[i]!=0) return false;
-11        }
-12        return true;
-13    }
-14}
+3        int[] freq = new int[26];
+4        if (s.length() != t.length())
+5            return false;
+6        for (int i = 0; i < s.length(); i++) {
+7            freq[s.charAt(i) - 'a']++;
+8            freq[t.charAt(i) - 'a']--;
+9        }
+10        for (int i = 0; i < 26; i++) {
+11            if (freq[i] != 0)
+12                return false;
+13        }
+14        return true;
+15    }
+16}
