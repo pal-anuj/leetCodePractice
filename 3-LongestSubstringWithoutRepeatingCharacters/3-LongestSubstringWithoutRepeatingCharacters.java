69a8157ffee40c8715b75dcb4e26679ce3a9fef0
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-04-16 06:18:46 +0530

    File updated at 16/04/2026, 06:18:46

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,26 +1,37 @@
-// Last updated: 16/04/2026, 06:01:24
+// Last updated: 16/04/2026, 06:18:46
 1class Solution {
 2    public int lengthOfLongestSubstring(String s) {
 3        int maxL = 0;
 4
 5        // Efficient Approach
-6        // int left = 0;
-7        // HashMap<Character, Integer> lastSeenIndex = new HashMap<>();
+6        int left = 0;
+7        HashMap<Character, Integer> lastSeenIndex = new HashMap<>();
 8
-9        for(int i=0;i<s.length();i++){
-10            int count=1;
-11            Set<Character> set= new HashSet<>();
-12            set.add(s.charAt(i));
-13            for(int j=i+1;j<s.length();j++){
-14                if(set.contains(s.charAt(j)))
-15                    break;
-16                else
-17                    count++;
-18                set.add(s.charAt(j)); 
-19            }
-20            maxL= Math.max(maxL, count);
-21        }
-22
-23        return maxL;
-24    }
-25}
+9        for (int right = 0; right < s.length(); right++) {
+10            char c = s.charAt(right);
+11            if (lastSeenIndex.containsKey(c)) {
+12                left = Math.max(left, lastSeenIndex.get(c)+1);
+13            }
+14
+15            lastSeenIndex.put(c, right);
+16            maxL = Math.max(maxL, right - left + 1);
+17        }
+18
+19        // // brute force
+20        // for(int i=0;i<s.length();i++){
+21        //     int count=1;
+22        //     Set<Character> set= new HashSet<>();
+23        //     set.add(s.charAt(i));
+24        //     for(int j=i+1;j<s.length();j++){
+25        //         if(set.contains(s.charAt(j)))
+26        //             break;
+27        //         else
+28        //             count++;
+29        //         set.add(s.charAt(j)); 
+30        //     }
+31        //     maxL= Math.max(maxL, count);
+32        // }
+33
+34        return maxL;
+35    }
+36}
