82fb13e779a52f2703ca76ffa41b9898ba2bbf73
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-04-16 05:36:48 +0530

    Create 1171-ShortestPathInBinaryMatrix

---
+++ 1171-ShortestPathInBinaryMatrix/1171-ShortestPathInBinaryMatrix.java
@@ -0,0 +1,43 @@
+// Last updated: 16/04/2026, 05:36:48
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+
+        if (grid[0][0]==1 || grid[n - 1][n - 1] == 1)
+            return -1;
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] { 0, 0 });
+
+        grid[0][0] = 1;
+
+        // 8-directionally
+        int[][] dirs = {
+                { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 },
+                { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 }
+        };
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int r = cur[0];
+            int c = cur[1];
+            int dist = grid[r][c];
+
+            if (r == n - 1 && c == n - 1)
+                return dist;
+
+            for (int[] d : dirs) {
+
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr >= 0 && nc >= 0 && nr < n && nc < n &&
+                        grid[nr][nc] == 0) {
+                    grid[nr][nc] = dist + 1;
+                    q.offer(new int[] { nr, nc });
+                }
+            }
+        }
+        return -1;
+    }
+}
