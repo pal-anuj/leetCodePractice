83d8d4ae4f5b5fd5493797ebf24c30f78ea43aa4
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-04-16 06:01:24 +0530

    File updated at 16/04/2026, 06:01:24

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,23 +1,26 @@
-// Last updated: 16/04/2026, 05:35:03
-class Solution {
-    public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int num:nums){
-            set.add(num);
-        }
-        int longest = 0;
-
-        for(int num:set){
-            if(!set.contains(num - 1)){
-                int curr = num;
-                int count = 1;
-            while(set.contains(curr + 1)){
-                curr++;
-                count++;
-            }
-            longest =  Math.max(longest,count);
-        }
-        }
-        return longest;
-    }
-}
+// Last updated: 16/04/2026, 06:01:24
+1class Solution {
+2    public int lengthOfLongestSubstring(String s) {
+3        int maxL = 0;
+4
+5        // Efficient Approach
+6        // int left = 0;
+7        // HashMap<Character, Integer> lastSeenIndex = new HashMap<>();
+8
+9        for(int i=0;i<s.length();i++){
+10            int count=1;
+11            Set<Character> set= new HashSet<>();
+12            set.add(s.charAt(i));
+13            for(int j=i+1;j<s.length();j++){
+14                if(set.contains(s.charAt(j)))
+15                    break;
+16                else
+17                    count++;
+18                set.add(s.charAt(j)); 
+19            }
+20            maxL= Math.max(maxL, count);
+21        }
+22
+23        return maxL;
+24    }
+25}
