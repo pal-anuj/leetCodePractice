ab6fb9b2c990d511c8d39568246f065d38bbc9de
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-04-18 21:15:34 +0530

    File updated at 18/04/2026, 21:15:33

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,43 +1,21 @@
-// Last updated: 18/04/2026, 21:11:40
-1class Solution {
-2    public int totalFruit(int[] fruits) {
-3        int n = fruits.length;
-4        int max = 0;
-5        // efficient approach
-6        HashMap<Integer, Integer> map = new HashMap<>();
-7        int l = 0;
-8        for (int r = 0; r < fruits.length; r++) {
-9            map.put(fruits[r], map.getOrDefault(fruits[r], 0) + 1);
-10
-11            if (map.size() > 2) {
-12                while (map.size() > 2) {
-13                    map.put(fruits[l], map.get(fruits[l]) - 1);
-14                    if (map.get(fruits[l]) == 0)
-15                        map.remove(fruits[l]);
-16                    l++;
-17                }
-18            } else {
-19                int count = 0;
-20                for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-21                    count += entry.getValue();
-22                }
-23                max = Math.max(max, count);
-24            }
-25        }
-26
-27        // // brute force time: O(n2)
-28        // for (int i = 0; i < n; i++) {
-29        //     Set<Integer> set = new HashSet<>();
-30        //     int count = 0;
-31        //     for (int j = i; j < n; j++) {
-32        //         set.add(fruits[j]);
-33        //         if (set.size() > 2)
-34        //             break;
-35        //         count++;
-36        //     }
-37        //     max = Math.max(max, count);
-38        // }
-39
-40        return max;
-41    }
-42}
+// Last updated: 18/04/2026, 21:15:33
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int maxFruits = 0;
+        Map<Integer, Integer> basket = new HashMap<>();
+        int left = 0;
+        for (int right = 0; right < fruits.length; right++) {
+            basket.put(fruits[right], basket.getOrDefault(fruits[right], 0) + 1);
+            while (basket.size() > 2) {
+                basket.put(fruits[left], basket.get(fruits[left]) - 1);
+                if (basket.get(fruits[left]) == 0) basket.remove(fruits[left]);
+                left++;
+            }
+            maxFruits = Math.max(maxFruits, right - left + 1);
+        }
+        
+
+        
+        return maxFruits;
+    }
+}
