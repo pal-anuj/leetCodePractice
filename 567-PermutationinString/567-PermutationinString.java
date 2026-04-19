e0b8257813d41e8e416750d495f4824ce723fc3d
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-04-19 22:41:02 +0530

    File updated at 19/04/2026, 22:41:02

--- 567-PermutationinString/567-PermutationinString.java
+++ 567-PermutationinString/567-PermutationinString.java
@@ -1,31 +1,50 @@
-// Last updated: 19/04/2026, 22:23:03
-1class Solution {
-2    public boolean checkInclusion(String s1, String s2) {
-3        int n = s1.length();
-4        int m = s2.length();
-5        System.out.println("n:" + n);
-6        System.out.println("m:" + m);
-7
-8        int[] freqCount = new int[26];
-9        for (int i = 0; i < n; i++) {
-10            freqCount[s1.charAt(i) - 'a']++;
-11        }
-12
-13        for (int i = 0; i <= m - n; i++) {
-14            int[] copy = freqCount.clone();
-15            for (int j = i; j < (i + n) && (j < m); j++) {
-16                copy[s2.charAt(j) - 'a']--;
-17            }
-18            int k;
-19            for (k = 0; k < 26; k++) {
-20                if (copy[k] == 0)
-21                    continue;
-22                else
-23                    break;
-24            }
-25            if (k == 26)
-26                return true;
-27        }
-28        return false;
-29    }
-30}
+// Last updated: 19/04/2026, 22:41:02
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : s1.toCharArray()) {
+            map.put(c, map.getOrDefault(c,0) + 1);
+        }
+
+        int right = 0;
+        int left = 0;
+        int count = map.size();
+        int k = s1.length();
+
+        while(right < s2.length()) {
+            char rightChar = s2.charAt(right);
+            
+            if(map.containsKey(rightChar)) {
+                map.put(rightChar, map.get(rightChar) - 1);
+
+                if(map.get(rightChar) == 0) {
+                    count--;
+                }
+            }
+
+            if(right - left + 1 < k) {
+                right++;
+            }
+            else if(right - left + 1 == k) {
+
+                if(count == 0) {
+                    return true;
+                }
+
+                char leftChar = s2.charAt(left);
+
+                if(map.containsKey(leftChar)) {
+                    map.put(leftChar, map.get(leftChar) + 1);
+
+                    if(map.get(leftChar) == 1) {
+                        count++;
+                    }
+                }
+                left++;
+                right++;
+            }
+
+        }
+        return false;
+    }
+}
