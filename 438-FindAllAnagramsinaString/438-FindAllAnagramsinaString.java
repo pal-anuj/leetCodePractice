4328a6d4ffeb0db1d3f176a40d2390bf81b87d59
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-04-24 07:55:19 +0530

    File updated at 24/04/2026, 07:55:19

--- 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
+++ 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
@@ -1,27 +1,64 @@
-// Last updated: 22/04/2026, 07:13:55
+// Last updated: 24/04/2026, 07:55:19
 1class Solution {
-2    public List<Integer> findAnagrams(String s, String p) {
-3        int[] freq = new int[26];
-4        for (char c : p.toCharArray()) {
-5            freq[c - 'a']++;
-6        }
-7
-8        List<Integer> res = new ArrayList<>();
-9        int left = 0;
-10        int right = 0;
-11        int count = p.length();
-12
-13        while (right < s.length()) {
-14            if (freq[s.charAt(right++) - 'a']-- > 0)
-15                count--;
-16
-17            if (count == 0)
-18                res.add(left);
-19
-20            if (right - left == p.length() && freq[s.charAt(left++) - 'a']++ >= 0)
-21                count++;
-22        }
-23
-24        return res;
-25    }
-26}
+2    public String minWindow(String s, String t) {
+3        int n = s.length();
+4        int m = t.length();
+5
+6        if (m > n)
+7            return "";
+8
+9        int[] freq = new int[256];
+10        for (char c : t.toCharArray()) {
+11            freq[c]++;
+12        }
+13
+14        // Efficeint Approach Time Comeplexity: O(2n+m), Space Complexity: O(256)
+15        int l = 0;
+16        int r = 0;
+17        int count = 0;
+18        int minLen = Integer.MAX_VALUE;
+19        int startIdx = -1;
+20        while (r < n) {
+21            if (freq[s.charAt(r)] > 0) {
+22                count++;
+23            }
+24            freq[s.charAt(r)]--;
+25
+26            while (count == m) {
+27                if (r - l + 1 < minLen) {
+28                    minLen = r - l + 1;
+29                    startIdx = l;
+30                }
+31
+32                freq[s.charAt(l)]++;
+33                if (freq[s.charAt(l)] > 0)
+34                    count = count - 1;
+35                l++;
+36            }
+37            r = r + 1;
+38        }
+39        return startIdx == -1 ? "" : s.substring(startIdx, startIdx + minLen);
+40
+41        // // Brute Force O(n2)
+42        // int startIdx = 0;
+43        // int endIdx = n;
+44        // boolean stringFound = false;
+45        // for (int i = 0; i <= n - m; i++) {
+46        //     int count = m;
+47        //     int[] temp = freq.clone();
+48        //     for (int j = i; j < n; j++) {
+49        //         if (temp[s.charAt(j)]-- > 0)
+50        //             count--;
+51        //         if (count == 0) {
+52        //             if (endIdx - startIdx > j - i) {
+53        //                 startIdx = i;
+54        //                 endIdx = j + 1;
+55        //             }
+56        //             stringFound = true;
+57        //             break;
+58        //         }
+59        //     }
+60        // }
+61        // return stringFound ? s.substring(startIdx, endIdx) : "";
+62    }
+63}
