c75219650dbd73aeca6aca0ef8df4273c844066f
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-04-25 10:39:52 +0530

    File updated at 25/04/2026, 10:39:51

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
@@ -1,11 +1,11 @@
-// Last updated: 25/04/2026, 10:37:37
+// Last updated: 25/04/2026, 10:39:51
 1class Solution {
 2    public int[] productExceptSelf(int[] nums) {
 3        int n = nums.length;
-4        int[] leftPrefix = new int[n+1];
+4        int[] leftPrefix = new int[n];
 5        leftPrefix[0] = 1;
-6        for (int i = 0; i < n; i++) {
-7            leftPrefix[i+1] = leftPrefix[i] * nums[i];
+6        for (int i = 1; i < n; i++) {
+7            leftPrefix[i] = leftPrefix[i-1] * nums[i-1];
 8        }
 9
 10        int[] res = new int[n];
@@ -14,7 +14,7 @@
 13            res[i]= leftPrefix[i] * rightSuffix;  
 14            rightSuffix *= nums[i];
 15        }
-16        
+16
 17        return res;
 18    }
 19}
