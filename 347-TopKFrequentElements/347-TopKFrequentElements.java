d4c3e2122f4572a456d973157da75dbc211271b7
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-04-30 08:56:16 +0530

    File updated at 30/04/2026, 08:56:15

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,20 +1,27 @@
-// Last updated: 29/04/2026, 23:13:29
+// Last updated: 30/04/2026, 08:56:15
 1class Solution {
-2    public boolean isValid(String s) {
-3        Stack<Character> st = new Stack<>();
-4        for (int i = 0; i < s.length(); i++) {
-5            char ch = s.charAt(i);
-6            if (ch == '(')
-7                st.push(')');
-8            else if (ch == '{')
-9                st.push('}');
-10            else if (ch == '[')
-11                st.push(']');
-12            else {
-13                if (st.isEmpty() || st.pop() != ch)
-14                    return false;
-15            }
-16        }
-17        return st.isEmpty();
-18    }
-19}
+2    public int[] dailyTemperatures(int[] temp) {
+3        int n = temp.length;
+4        int[] res = new int[n];
+5        Stack<Integer> st = new Stack<>();
+6        for (int i = n - 1; i >= 0; i--) {
+7            while (!st.isEmpty() && temp[i] >= temp[st.peek()]) {
+8                st.pop();
+9            }
+10
+11            res[i] = st.isEmpty() ? 0 : st.peek() - i;
+12            st.push(i);
+13        }
+14
+15        // // brute force
+16        // for (int i = 0; i < n; i++) {
+17        //     for (int j = i; j < n; j++) {
+18        //         if (temp[i] < temp[j]) {
+19        //             res[i] = j - i;
+20        //             break;
+21        //         }
+22        //     }
+23        // }
+24        return res;
+25    }
+26}
