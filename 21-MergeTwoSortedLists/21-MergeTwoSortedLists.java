4b7cd55d13c3145f695ce36f7941e34caadf2534
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-05-03 22:38:31 +0530

    File updated at 03/05/2026, 22:38:30

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -1,4 +1,4 @@
-// Last updated: 04/01/2026, 12:53:22
+// Last updated: 03/05/2026, 22:38:30
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -11,23 +11,31 @@
 10 */
 11class Solution {
 12    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-13
-14        ListNode res= new ListNode(0);
-15        ListNode curr= res;            
-16        while(list1 != null || list2 != null){
-17
-18            if(list2==null || (list1!=null && list1.val <= list2.val)){
-19                curr.next= new ListNode(list1.val);
-20                curr= curr.next;
-21                list1= list1.next;
-22            }
-23            else{
-24                curr.next= new ListNode(list2.val);
-25                curr= curr.next;
-26                list2= list2.next;
-27            }
-28        }
-29
-30        return res.next;
-31    }
-32}
+13        
+14        ListNode newHead= new ListNode();
+15        ListNode cur= newHead;
+16        while(list1!=null && list2!=null){
+17            if(list1.val <= list2.val){
+18                cur.next= new ListNode(list1.val);
+19                cur= cur.next;
+20                list1= list1.next;
+21            }else{
+22                cur.next= new ListNode(list2.val);
+23                cur= cur.next;
+24                list2= list2.next;
+25            }
+26        }
+27
+28        while(list1!=null){
+29            cur.next= list1;
+30            cur=cur.next;
+31            list1= list1.next;
+32        }
+33        while(list2!=null){
+34            cur.next= list2;
+35            cur=cur.next;
+36            list2= list2.next;
+37        }
+38        return newHead.next;
+39    }
+40}
