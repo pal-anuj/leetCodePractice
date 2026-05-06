c4a0086511d020f8737fd0ac54e694969b47b0cd
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-05-06 22:45:03 +0530

    File updated at 06/05/2026, 22:45:03

--- 35-SearchInsertPosition/35-SearchInsertPosition.java
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -1,20 +1,32 @@
-// Last updated: 06/05/2026, 22:30:49
+// Last updated: 06/05/2026, 22:45:03
 1class Solution {
-2    public int searchInsert(int[] nums, int target) {
-3        int l=0;
-4        int r=nums.length-1;
-5        int res=nums.length;
-6        while(l<=r){
-7            int mid= l+(r-l)/2;
-8            if(nums[mid] >= target){
-9                res=mid;
-10                r= mid-1;
-11            }
-12            else{
-13                l= mid+1;
-14            }
-15        }
-16        return res;
-17
-18    }
-19}
+2    public int[] searchRange(int[] nums, int target) {
+3        int n = nums.length;
+4        int left = 0;
+5        int right = nums.length - 1;
+6
+7        int leftidx = -1;
+8        while (left <= right) {
+9            int mid = left + (right - left) / 2;
+10            if (nums[mid] == target) {
+11                leftidx = mid;
+12                right = mid - 1;
+13            } else if (nums[mid] > target) {
+14                right = mid - 1;
+15            } else {
+16                left = mid + 1;
+17            }
+18        }
+19        int rightidx = leftidx;
+20        for (int i = leftidx + 1; i < n; i++) {
+21            if (nums[i] != target)
+22                break;
+23            else
+24                rightidx = i;
+25        }
+26        int res[] = new int[2];
+27        res[0] = leftidx;
+28        res[1] = rightidx;
+29        return res;
+30    }
+31}
