ec1f44ff005e2199cf6350026242fa27b72905dc
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-05-15 09:21:29 +0530

    File updated at 15/05/2026, 09:21:29

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
@@ -1,28 +1,27 @@
-// Last updated: 15/05/2026, 09:15:13
+// Last updated: 15/05/2026, 09:21:29
 1class Solution {
 2
-3    /*
-4    [4,5,6,7,0,1,2] l=0, r=7 mid= 7/2=> 3
-5    res=7
-6    nums[l] > nums[mid]
-7    
-8    
-9     */
-10    public int findMin(int[] nums) {
-11        int l = 0;
-12        int r = nums.length - 1;
-13        int res = Integer.MAX_VALUE;
-14        while (l <= r) {
-15            int mid = l + (r - l) / 2;
-16            if (nums[l] <= nums[mid]) {
-17                res = Math.min(res, nums[l]);
-18                l = mid + 1;
-19            } else {
-20                r = mid - 1;
-21                res = Math.min(res, nums[mid]);
-22            }
-23
-24        }
-25        return res;
-26    }
-27}
+3    public int findMin(int[] nums) {
+4        int l = 0;
+5        int r = nums.length - 1;
+6        int res = Integer.MAX_VALUE;
+7        while (l <= r) {
+8            int mid = l + (r - l) / 2;
+9
+10            if(nums[l] <= nums[r]){
+11                res= Math.min(res, nums[l]);
+12                break;
+13            }
+14
+15            if (nums[l] <= nums[mid]) {
+16                res = Math.min(res, nums[l]);
+17                l = mid + 1;
+18            } else {
+19                r = mid - 1;
+20                res = Math.min(res, nums[mid]);
+21            }
+22
+23        }
+24        return res;
+25    }
+26}
