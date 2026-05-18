d80e7c1b2f22295feab0d5cd9ea06bf19e940927
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-05-18 23:53:56 +0530

    File updated at 18/05/2026, 23:53:56

--- 875-KokoEatingBananas/875-KokoEatingBananas.java
+++ 875-KokoEatingBananas/875-KokoEatingBananas.java
@@ -1,51 +1,35 @@
-// Last updated: 16/05/2026, 08:56:04
+// Last updated: 18/05/2026, 23:53:56
 1class Solution {
-2    public boolean canFinish(int[] piles, int k, int h) {
-3        long hours = 0;
-4        for (int p : piles) {
-5            hours += (p + k - 1) / k;
-6            // hours += Math.ceil((double) piles[j] / k);
-7        }
-8        return hours <= h;
-9    }
-10
-11    public int minEatingSpeed(int[] piles, int h) {
-12        int maxK = Integer.MIN_VALUE;
-13        for (int p : piles)
-14            maxK = Math.max(maxK, p);
-15
-16        int l = 1;
-17        int r = maxK;
-18        while (l < r) {
-19            int mid = l + (r - l) / 2;
-20            if (canFinish(piles, mid, h)) {
-21                r = mid;
-22            } else
-23                l = mid + 1;
-24        }
-25        return l;
-26
-27        // int minK= maxK;
-28        // while (l <= r) {
-29        //     int mid = l + (r - l) / 2;
-30        //     if (canFinish(piles, mid, h)) {
-31        //         minK = mid;  // 
-32        //         r = mid - 1;
-33        //     } else
-34        //         l = mid + 1;
-35        // }
-36        // return minK;
-37
-38        // // brute force
-39
-40        // for (int i = maxK; i >= 1; i--) {
-41        //     double minH = 0;
-42        //     for (int j = 0; j < n; j++) {
-43        //         minH += Math.ceil((double) piles[j] / i);
-44        //     }
-45        //     if (minH <= h)
-46        //         minK = i;
-47        // }
-48        // return minK;
-49    }
-50}
+2    public int mySqrt(int x) {
+3        int res = 0;
+4        int l = 1;
+5        int h = x;
+6        if (x == 0)
+7            return 0;
+8        while (l <= h) {
+9            int mid = l + (h - l) / 2;
+10            long sqr = (long) mid * mid;
+11            if (sqr == x)
+12                return mid;
+13            else if (sqr > x) {
+14                h = mid - 1;
+15            } else {
+16                res = mid;
+17                l = mid + 1;
+18            }
+19        }
+20        return res;
+21
+22        // // Brute force approach
+23        // for (int i = 1; i <= x; i++) {
+24        //     int sqr = i * i;
+25        //     if (sqr == x)
+26        //         return i;
+27        //     else if (sqr < x)
+28        //         res = i;
+29        //     else
+30        //         return res;
+31        // }
+32        // return res;
+33    }
+34}
