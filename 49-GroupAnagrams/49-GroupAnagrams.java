5c1a7c60413ca423e7d6a1da8f0e4193c79e1b4b
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-05-20 12:27:15 +0530

    File updated at 20/05/2026, 12:27:14

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,23 +1,25 @@
-// Last updated: 16/04/2026, 05:46:51
+// Last updated: 20/05/2026, 12:27:14
 1class Solution {
 2    public List<List<String>> groupAnagrams(String[] strs) {
-3        Map<String, List<String>> group = new HashMap<>();
+3        Map<String, List<String>> map = new HashMap<>();
 4        for (String str : strs) {
-5            char[] charArr = str.toCharArray();
-6            Arrays.sort(charArr);
-7            String key = new String(charArr);
-8
-9            group.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
-10            // if (group.containsKey(key)) {
-11            //     List<String> ls = group.get(key);
-12            //     ls.add(str);
-13            // } else {
-14            //     List<String> ls = new ArrayList<>();
-15            //     ls.add(str);
-16            //     group.put(key, ls);
-17            // }
+5            char[] arr = str.toCharArray();
+6            Arrays.sort(arr);
+7
+8            String key = String.valueOf(arr);
+9            // System.out.println(key);
+10            if (map.containsKey(key)) {
+11                List<String> ls = map.get(key);
+12                ls.add(str);
+13            } else {
+14                List<String> ls = new ArrayList<>();
+15                ls.add(str);
+16                map.put(key, ls);
+17            }
 18        }
 19
-20        return new ArrayList<>(group.values());
-21    }
-22}
+20        List<List<String>> res = new ArrayList<>();
+21        map.forEach((key, value) -> res.add(value));
+22        return res;
+23    }
+24}
