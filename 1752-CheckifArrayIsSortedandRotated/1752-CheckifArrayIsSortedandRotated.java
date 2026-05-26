3908f1b4912dc10904dc4ea81175bf09536be91c
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-05-26 23:47:53 +0530

    File updated at 26/05/2026, 23:47:53

--- 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.java
+++ 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.java
@@ -1,62 +1,14 @@
-// Last updated: 15/07/2025, 00:37:37
-class Solution {
-    public boolean check(int[] arr) {
-
-        // //GFG solution
-        // int n= arr.length;
-        // if(arr[n-1]<arr[0]){
-        //     int pivotalPoint=0;
-        //     for(int i=0; i<n-1; i++){
-        //         if(arr[i+1] < arr[i])
-        //             pivotalPoint++;
-        //     }
-        //     if(pivotalPoint==1) 
-        //         return true; 
-        // }else if(arr[n-1] > arr[0]){
-        //     int pivotalPoint=0;
-        //     for(int i=0; i<n-1; i++){
-        //         if(arr[i+1] > arr[i])
-        //             pivotalPoint++;
-        //     }
-        //     if(pivotalPoint==1) 
-        //         return true;
-        // }
-        // return false;
-
-        //My solution
-        int n= arr.length;
-        int j;
-        //Checking if array is only sorted in Asc order
-        for(j=1; j<n; j++){
-            if(arr[j] < arr[j-1])
-                break;
-        }
-        if(j==n)
-            return true;
-
-        // //Checking if array is only sorted in desc order
-        // for(j=1; j<n; j++){
-        //     if(arr[j] > arr[j-1])
-        //         break;
-        // }
-        // if(j==n)
-        //     return false;
-        
-        int asc=0;
-        int dsc=0;
-        for(int i=0; i<n; i++){
-            if(arr[i] > arr[(i+1) % n])
-                asc++;
-            // if(arr[i] < arr[(i+1) % n])
-            //     dsc++;
-        }
-        return (asc==1 );
-    }
-
-    //  public boolean check(int[] nums) {
-    //     int count = 0, n = nums.length;
-    //     for (int i = 0; i < n; i++) 
-    //         if (nums[i] > nums[(i+1) % n] && ++count > 1) return false;
-    //     return count <= 1;
-    // }
-}
+// Last updated: 26/05/2026, 23:47:53
+1class Solution {
+2    public boolean check(int[] nums) {
+3        int n= nums.length;
+4        int count = 0;
+5        for (int i = 0; i < nums.length; i++) {
+6            if (nums[i] > nums[(i + 1) % n]) {
+7                count++;
+8            }
+9        }
+10
+11        return count <= 1;
+12    }
+13}
