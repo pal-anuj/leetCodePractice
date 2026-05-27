aa7f74ad3586003f2c4fc1da24aa378f90aae795
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-05-28 01:32:29 +0530

    File updated at 28/05/2026, 01:32:29

--- 3121-CounttheNumberofSpecialCharactersII/3121-CounttheNumberofSpecialCharactersII.java
+++ 3121-CounttheNumberofSpecialCharactersII/3121-CounttheNumberofSpecialCharactersII.java
@@ -1,41 +1,49 @@
-// Last updated: 28/05/2026, 01:25:45
+// Last updated: 28/05/2026, 01:32:29
 1class Solution {
 2    public int numberOfSpecialChars(String word) {
 3        int n = word.length();
 4        int count = 0;
 5        for (char ch = 'a'; ch <= 'z'; ch++) {
-6            boolean ignoreChar= false;
-7            for (int j = n - 1; j >= 0; j--) {
-8                if (ch == word.charAt(j))
-9                    break;
-10                if (Character.toUpperCase(ch) == word.charAt(j)) {
-11                    j = j - 1;
-12                    boolean validLower = false;
-13                    while (j >= 0) {
-14                        if (validLower && Character.toUpperCase(ch) == word.charAt(j)) {
-15                            ignoreChar= true;
-16                            validLower = false;
-17                            System.out.println("Anuj=> "+j);
-18                            break;
-19                        }
-20
-21                        if (word.charAt(j) == ch) {
-22                            // System.out.println(j);
-23                            validLower = true;
-24                        }
-25                        j--;
-26                    }
-27                    if (validLower) {
-28                        System.out.println(ch + ", j=>"+j);
-29                        count++;
-30                        break;
-31                    }
-32
-33                    if(ignoreChar)
-34                        break;
-35                }
-36            }
-37        }
-38        return count;
-39    }
-40}
+6            boolean ignoreChar = false;
+7            // efficient approach
+8            int lastLower = word.lastIndexOf(ch);
+9            int firstUpper = word.indexOf(Character.toUpperCase(ch));
+10
+11            if (lastLower != -1 && firstUpper != -1 && lastLower < firstUpper)
+12                count++;
+13
+14            // brute force O(26 * n)
+15            // for (int j = n - 1; j >= 0; j--) {
+16            //     if (ch == word.charAt(j))
+17            //         break;
+18            //     if (Character.toUpperCase(ch) == word.charAt(j)) {
+19            //         j = j - 1;
+20            //         boolean validLower = false;
+21            //         while (j >= 0) {
+22            //             if (validLower && Character.toUpperCase(ch) == word.charAt(j)) {
+23            //                 ignoreChar = true;
+24            //                 validLower = false;
+25            //                 System.out.println("Anuj=> " + j);
+26            //                 break;
+27            //             }
+28
+29            //             if (word.charAt(j) == ch) {
+30            //                 // System.out.println(j);
+31            //                 validLower = true;
+32            //             }
+33            //             j--;
+34            //         }
+35            //         if (validLower) {
+36            //             System.out.println(ch + ", j=>" + j);
+37            //             count++;
+38            //             break;
+39            //         }
+40
+41            //         if (ignoreChar)
+42            //             break;
+43            //     }
+44            // }
+45        }
+46        return count;
+47    }
+48}
