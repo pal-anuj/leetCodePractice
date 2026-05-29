5e02a84106c8dc4d8c1cb7686c5eeb94001945b2
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-05-29 09:52:12 +0530

    File updated at 29/05/2026, 09:52:12

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,25 +1,19 @@
-// Last updated: 21/05/2026, 23:47:51
+// Last updated: 29/05/2026, 09:52:12
 1class Solution {
-2    public int[] topKFrequent(int[] nums, int k) {
-3        HashMap<Integer, Integer> freqCount = new HashMap<>();
-4        for (int num : nums) {
-5            freqCount.put(num, freqCount.getOrDefault(num, 0) + 1);
-6        }
-7
-8        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-9        for (Map.Entry<Integer, Integer> entry : freqCount.entrySet()) {
-10            pq.offer(new int[] { entry.getKey(), entry.getValue() });
-11
-12            if (pq.size() > k)
-13                pq.poll();
-14        }
-15
-16        int[] res = new int[pq.size()];
-17        int i = 0;
-18        while (!pq.isEmpty()) {
-19            res[i++] = pq.poll()[0];
-20        }
-21
-22        return res;
-23    }
-24}
+2    public int[] productExceptSelf(int[] nums) {
+3        int n = nums.length;
+4        int[] lprefix = new int[n + 1];
+5        lprefix[0] = 1;
+6        for (int i = 0; i < n; i++) {
+7            lprefix[i + 1] = lprefix[i] * nums[i];
+8        }
+9
+10        int rsuffix = 1;
+11        int[] res = new int[n];
+12        for (int i = n - 1; i >= 0; i--) {
+13            res[i] = lprefix[i] * rsuffix;
+14            rsuffix *= nums[i];
+15        }
+16        return res;
+17    }
+18}
