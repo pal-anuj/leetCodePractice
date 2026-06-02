ed032dd86d1f900837e39359cbc397908c0c2e56
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-06-03 00:33:00 +0530

    File updated at 03/06/2026, 00:33:00

--- 3633-EarliestFinishTimeforLandandWaterRidesI/3633-EarliestFinishTimeforLandandWaterRidesI.java
+++ 3633-EarliestFinishTimeforLandandWaterRidesI/3633-EarliestFinishTimeforLandandWaterRidesI.java
@@ -1,35 +1,44 @@
-// Last updated: 03/06/2026, 00:31:04
-1class Solution {
-2    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-3
-4        int n = landStartTime.length;
-5        int m = waterStartTime.length;
-6        int totalTime = Integer.MAX_VALUE;
-7
-8        for (int i = 0; i < n; i++) {
-9            for (int j = 0; j < m; j++) {
-10                int curTotalTime = 0;
-11                if (landStartTime[i] < waterStartTime[j]) {
-12                    curTotalTime = landStartTime[i] + landDuration[i];
-13                    if (waterStartTime[j] < curTotalTime) {
-14                        curTotalTime += waterDuration[j];
-15                    } else {
-16                        int diff = waterStartTime[j] - curTotalTime;
-17                        curTotalTime += diff + waterDuration[j];
-18                    }
-19                    totalTime = Math.min(totalTime, curTotalTime);
-20                } else {
-21                    curTotalTime = waterStartTime[j] + waterDuration[j];
-22                    if (landStartTime[i] < curTotalTime) {
-23                        curTotalTime += landDuration[i];
-24                    } else {
-25                        int diff = landStartTime[i] - curTotalTime;
-26                        curTotalTime += diff + landDuration[i];
-27                    }
-28                    totalTime = Math.min(totalTime, curTotalTime);
-29                }
-30            }
-31        }
-32        return totalTime;
-33    }
-34}
+// Last updated: 03/06/2026, 00:33:00
+class Solution {
+
+    private int solve(
+        int[] start1,
+        int[] duration1,
+        int[] start2,
+        int[] duration2
+    ) {
+        int finish1 = Integer.MAX_VALUE;
+        for (int i = 0; i < start1.length; i++) {
+            finish1 = Math.min(finish1, start1[i] + duration1[i]);
+        }
+        int finish2 = Integer.MAX_VALUE;
+        for (int i = 0; i < start2.length; i++) {
+            finish2 = Math.min(
+                finish2,
+                Math.max(start2[i], finish1) + duration2[i]
+            );
+        }
+        return finish2;
+    }
+
+    public int earliestFinishTime(
+        int[] landStartTime,
+        int[] landDuration,
+        int[] waterStartTime,
+        int[] waterDuration
+    ) {
+        int land_water = solve(
+            landStartTime,
+            landDuration,
+            waterStartTime,
+            waterDuration
+        );
+        int water_land = solve(
+            waterStartTime,
+            waterDuration,
+            landStartTime,
+            landDuration
+        );
+        return Math.min(land_water, water_land);
+    }
+}
