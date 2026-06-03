f58c5a07f2261ce600d93ac48c2e6fb8adfab4ce
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-06-03 23:32:50 +0530

    File updated at 03/06/2026, 23:32:50

--- 3633-EarliestFinishTimeforLandandWaterRidesI/3633-EarliestFinishTimeforLandandWaterRidesI.java
+++ 3633-EarliestFinishTimeforLandandWaterRidesI/3633-EarliestFinishTimeforLandandWaterRidesI.java
@@ -1,44 +1,28 @@
-// Last updated: 03/06/2026, 00:33:00
-class Solution {
-
-    private int solve(
-        int[] start1,
-        int[] duration1,
-        int[] start2,
-        int[] duration2
-    ) {
-        int finish1 = Integer.MAX_VALUE;
-        for (int i = 0; i < start1.length; i++) {
-            finish1 = Math.min(finish1, start1[i] + duration1[i]);
-        }
-        int finish2 = Integer.MAX_VALUE;
-        for (int i = 0; i < start2.length; i++) {
-            finish2 = Math.min(
-                finish2,
-                Math.max(start2[i], finish1) + duration2[i]
-            );
-        }
-        return finish2;
-    }
-
-    public int earliestFinishTime(
-        int[] landStartTime,
-        int[] landDuration,
-        int[] waterStartTime,
-        int[] waterDuration
-    ) {
-        int land_water = solve(
-            landStartTime,
-            landDuration,
-            waterStartTime,
-            waterDuration
-        );
-        int water_land = solve(
-            waterStartTime,
-            waterDuration,
-            landStartTime,
-            landDuration
-        );
-        return Math.min(land_water, water_land);
-    }
-}
+// Last updated: 03/06/2026, 23:32:50
+1class Solution {
+2    public int earliestFinishTime(int[] landStartTime, int[] landDuration,
+3            int[] waterStartTime, int[] waterDuration) {
+4
+5        int n = landStartTime.length;
+6        int m = waterStartTime.length;
+7
+8        int ans = Integer.MAX_VALUE;
+9
+10        for (int i = 0; i < n; i++) {
+11            for (int j = 0; j < m; j++) {
+12
+13                // Land -> Water
+14                int landFinish = landStartTime[i] + landDuration[i];
+15                int waterBegin = Math.max(landFinish, waterStartTime[j]);
+16                ans = Math.min(ans, waterBegin + waterDuration[j]);
+17
+18                // Water -> Land
+19                int waterFinish = waterStartTime[j] + waterDuration[j];
+20                int landBegin = Math.max(waterFinish, landStartTime[i]);
+21                ans = Math.min(ans, landBegin + landDuration[i]);
+22            }
+23        }
+24
+25        return ans;
+26    }
+27}
