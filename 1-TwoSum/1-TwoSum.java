2eedf6b7e6be9cf26855c9788609fbeb13c4dccc
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-06-06 20:41:09 +0530

    File updated at 06/06/2026, 20:41:08

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,17 +1,18 @@
-// Last updated: 25/05/2026, 00:06:40
+// Last updated: 06/06/2026, 20:41:08
 1class Solution {
 2    public int[] twoSum(int[] nums, int target) {
 3        int n = nums.length;
-4        Map<Integer,Integer> map= new HashMap<>();
-5        int[] res= new int[2];
-6        map.put(nums[0], 0);
-7        for(int i=1;i<n;i++){
-8            int num= target - nums[i];
-9            if(map.containsKey(num))
-10                return new int[]{map.get(num), i};
-11            map.put(nums[i], i);
-12        }
-13
-14        return res;
-15    }
-16}
+4        // bruet force
+5        int[] res = new int[2];
+6        for (int i = 0; i < n - 1; i++) {
+7            for (int j = i + 1; j < n; j++) {
+8                if (nums[i] + nums[j] == target) {
+9                    res[0] = i;
+10                    res[1] = j;
+11                    return res;
+12                }
+13            }
+14        }
+15        return res;
+16    }
+17}
