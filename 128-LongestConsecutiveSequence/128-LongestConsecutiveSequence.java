08f2b50b757afdc656a937a51310b4c55e3a18b1
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-06-07 10:42:03 +0530

    File updated at 07/06/2026, 10:42:03

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
@@ -1,20 +1,22 @@
-// Last updated: 25/04/2026, 10:39:51
+// Last updated: 07/06/2026, 10:42:03
 1class Solution {
-2    public int[] productExceptSelf(int[] nums) {
+2    public List<Integer> majorityElement(int[] nums) {
 3        int n = nums.length;
-4        int[] leftPrefix = new int[n];
-5        leftPrefix[0] = 1;
-6        for (int i = 1; i < n; i++) {
-7            leftPrefix[i] = leftPrefix[i-1] * nums[i-1];
-8        }
-9
-10        int[] res = new int[n];
-11        int rightSuffix = 1;
-12        for (int i = n-1; i >= 0; i--) {            
-13            res[i]= leftPrefix[i] * rightSuffix;  
-14            rightSuffix *= nums[i];
-15        }
-16
-17        return res;
-18    }
-19}
+4        Map<Integer, Integer> freqCount = new HashMap<>();
+5        for (int num : nums) {
+6            freqCount.put(num, freqCount.getOrDefault(num, 0) + 1);
+7        }
+8
+9        int maj = (int) Math.ceil((double) (n / 3));
+10        System.out.println(maj);
+11        List<Integer> res = new ArrayList<>();
+12        for (Map.Entry<Integer, Integer> entry : freqCount.entrySet()) {
+13            if (entry.getValue() > maj) {
+14                System.out.println("freq=>" + entry.getValue() + " , num= " + entry.getKey());
+15                res.add(entry.getKey());
+16            }
+17        }
+18
+19        return res;
+20    }
+21}
