7451633879832971e25d47a7029127385ce9aa2e
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-06-07 11:33:14 +0530

    File updated at 07/06/2026, 11:33:13

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
@@ -1,22 +1,50 @@
-// Last updated: 07/06/2026, 10:42:12
+// Last updated: 07/06/2026, 11:33:13
 1class Solution {
 2    public List<Integer> majorityElement(int[] nums) {
 3        int n = nums.length;
-4        Map<Integer, Integer> freqCount = new HashMap<>();
-5        for (int num : nums) {
-6            freqCount.put(num, freqCount.getOrDefault(num, 0) + 1);
-7        }
-8
-9        int maj = (int) Math.ceil((double) (n / 3));
-10        // System.out.println(maj);
-11        List<Integer> res = new ArrayList<>();
-12        for (Map.Entry<Integer, Integer> entry : freqCount.entrySet()) {
-13            if (entry.getValue() > maj) {
-14                // System.out.println("freq=>" + entry.getValue() + " , num= " + entry.getKey());
-15                res.add(entry.getKey());
-16            }
-17        }
-18
-19        return res;
-20    }
-21}
+4
+5        // > n/2  -> keep 1 candidate
+6        // > n/3  -> keep 2 candidates
+7        // > n/k  -> keep k-1 candidates
+8        // Boyer-Moore Voting Algorithm
+9        int count1 = 0;
+10        int candidate1 = 0;
+11        int count2 = 0;
+12        int candidate2 = 0;
+13        List<Integer> res = new ArrayList<>();
+14        for (int num : nums) {
+15            if (num == candidate1) {
+16                count1++;
+17            } else if (num == candidate2) {
+18                count2++;
+19            } else if (count1 == 0) {
+20                candidate1 = num;
+21                count1 = 1;
+22            } else if (count2 == 0) {
+23                candidate2 = num;
+24                count2 = 1;
+25            } else {
+26                count1--;
+27                count2--;
+28            }
+29        }
+30
+31        count1 = 0;
+32        count2 = 0;
+33        for (int num : nums) {
+34            if (num == candidate1) {
+35                count1++;
+36            } else if (num == candidate2) {
+37                count2++;
+38            }
+39        }
+40        // System.out.println(candidate1 + " -> " + count1);
+41        // System.out.println(candidate2 + " -> " + count2);
+42        if (count1 > n / 3)
+43            res.add(candidate1);
+44        if (count2 > n / 3)
+45            res.add(candidate2);
+46
+47        return res;
+48    }
+49}
