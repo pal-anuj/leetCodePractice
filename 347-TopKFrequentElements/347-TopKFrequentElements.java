ad2410e1987509b34abafb39b614ab8b0ebb9a83
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-06-10 19:02:15 +0530

    File updated at 10/06/2026, 19:02:15

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,27 +1,18 @@
-// Last updated: 30/04/2026, 08:56:15
+// Last updated: 10/06/2026, 19:02:15
 1class Solution {
-2    public int[] dailyTemperatures(int[] temp) {
-3        int n = temp.length;
-4        int[] res = new int[n];
-5        Stack<Integer> st = new Stack<>();
-6        for (int i = n - 1; i >= 0; i--) {
-7            while (!st.isEmpty() && temp[i] >= temp[st.peek()]) {
-8                st.pop();
-9            }
-10
-11            res[i] = st.isEmpty() ? 0 : st.peek() - i;
-12            st.push(i);
-13        }
-14
-15        // // brute force
-16        // for (int i = 0; i < n; i++) {
-17        //     for (int j = i; j < n; j++) {
-18        //         if (temp[i] < temp[j]) {
-19        //             res[i] = j - i;
-20        //             break;
-21        //         }
-22        //     }
-23        // }
-24        return res;
-25    }
-26}
+2    public int lengthOfLongestSubstring(String s) {
+3        int res = 0;
+4        int l = 0;
+5        HashMap<Character, Integer> map = new HashMap<>();
+6        for (int r = 0; r < s.length(); r++) {
+7            char ch = s.charAt(r);
+8            if (map.containsKey(ch)) {
+9                l = Math.max(l, map.get(ch) + 1);
+10            }
+11
+12            map.put(ch, r);
+13            res = Math.max(res, r - l + 1);
+14        }
+15        return res;
+16    }
+17}
