0a55be9c5c41873df36300231cf8e2b8bd81ea62
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-06-16 23:13:19 +0530

    File updated at 16/06/2026, 23:13:18

--- 3962-MaximumSubarraySumAfteratMostKSwaps/3962-MaximumSubarraySumAfteratMostKSwaps.java
+++ 3962-MaximumSubarraySumAfteratMostKSwaps/3962-MaximumSubarraySumAfteratMostKSwaps.java
@@ -1,4 +1,4 @@
-// Last updated: 16/06/2026, 23:12:52
+// Last updated: 16/06/2026, 23:13:18
 1class Solution {
 2    public int characterReplacement(String s, int k) {
 3        int n = s.length();
@@ -14,40 +14,39 @@
 13            freq[ch - 'A']++;
 14            maxf = Math.max(maxf, freq[ch - 'A']);
 15
-16            while ((r - l + 1) - maxf > k) {
+16            if ((r - l + 1) - maxf > k) {
 17                freq[s.charAt(l) - 'A']--;
 18                maxf = 0;
-19                for (int i = 0; i < 26; i++) {
-20                    maxf = Math.max(maxf, freq[i]);
-21                }
-22                System.out.println("Anuj=" + l);
-23                l++;
-24            }
-25
-26            if ((r - l + 1) - maxf <= k)
-27                maxlen = Math.max(maxlen, r - l + 1);
-28
-29            r++;
-30        }
-31        return maxlen;
-32
-33        // // Brute force approach
-34        // for (int i = 0; i < n; i++) {
-35        //     int[] freq = new int[26];
-36        //     int maxfreq= 0;
-37        //     for (int j = i; j < n; j++) {
-38
-39        //         char ch = s.charAt(j);
-40        //         freq[ch - 'A']++;
-41
-42        //         maxfreq = Math.max(maxfreq, freq[ch-'A']);
-43
-44        //         int len = j - i + 1;
-45        //         if (len - maxfreq <= k) {
-46        //             res = Math.max(res, len);
-47        //         }
-48        //     }
-49        // }
-50        // return res;
-51    }
-52}
+19                // for (int i = 0; i < 26; i++) {
+20                    // maxf = Math.max(maxf, freq[i]);
+21                // }
+22                l++;
+23            }
+24
+25            if ((r - l + 1) - maxf <= k)
+26                maxlen = Math.max(maxlen, r - l + 1);
+27
+28            r++;
+29        }
+30        return maxlen;
+31
+32        // // Brute force approach
+33        // for (int i = 0; i < n; i++) {
+34        //     int[] freq = new int[26];
+35        //     int maxfreq= 0;
+36        //     for (int j = i; j < n; j++) {
+37
+38        //         char ch = s.charAt(j);
+39        //         freq[ch - 'A']++;
+40
+41        //         maxfreq = Math.max(maxfreq, freq[ch-'A']);
+42
+43        //         int len = j - i + 1;
+44        //         if (len - maxfreq <= k) {
+45        //             res = Math.max(res, len);
+46        //         }
+47        //     }
+48        // }
+49        // return res;
+50    }
+51}
