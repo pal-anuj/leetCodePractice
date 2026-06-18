0f0208234197dc24efc52bef54c4bfb7ea39e35a
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-06-18 23:07:43 +0530

    File updated at 18/06/2026, 23:07:42

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,21 +1,26 @@
-// Last updated: 18/04/2026, 21:15:33
-class Solution {
-    public int totalFruit(int[] fruits) {
-        int maxFruits = 0;
-        Map<Integer, Integer> basket = new HashMap<>();
-        int left = 0;
-        for (int right = 0; right < fruits.length; right++) {
-            basket.put(fruits[right], basket.getOrDefault(fruits[right], 0) + 1);
-            while (basket.size() > 2) {
-                basket.put(fruits[left], basket.get(fruits[left]) - 1);
-                if (basket.get(fruits[left]) == 0) basket.remove(fruits[left]);
-                left++;
-            }
-            maxFruits = Math.max(maxFruits, right - left + 1);
-        }
-        
-
-        
-        return maxFruits;
-    }
-}
+// Last updated: 18/06/2026, 23:07:42
+1class Solution {
+2    public int longestSubstring(String s, int k) {
+3        int maxlen = 0;
+4        int curlen = 1;
+5
+6        for (int i = 0; i < s.length(); i++) {
+7            int[] freq = new int[26];
+8            for (int j = i; j < s.length(); j++) {
+9                freq[s.charAt(j) - 'a']++;
+10                boolean valid = true;
+11                for (int c : freq) {
+12                    if (c > 0 && c < k) {
+13                        valid = false;
+14                        break;
+15                    }
+16                }
+17
+18                if (valid) {
+19                    maxlen = Math.max(maxlen, j - i + 1);
+20                }
+21            }
+22        }
+23        return maxlen;
+24    }
+25}
