df21cb11dd589b58384dd7097b601a1440b1098f
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-06-19 20:40:51 +0530

    File updated at 19/06/2026, 20:40:50

--- 2931-MaximumSpendingAfterBuyingItems/2931-MaximumSpendingAfterBuyingItems.java
+++ 2931-MaximumSpendingAfterBuyingItems/2931-MaximumSpendingAfterBuyingItems.java
@@ -1,34 +1,36 @@
-// Last updated: 01/06/2026, 00:12:42
+// Last updated: 19/06/2026, 20:40:50
 1class Solution {
-2    public int evalRPN(String[] tokens) {
-3        Stack<Integer> st = new Stack<>();
-4        int sum = 1;
-5        for (String token : tokens) {
-6            if ("+".equals(token) || "-".equals(token) || "*".equals(token)
-7                    || "/".equals(token)) {
-8
-9                int b = st.pop();
-10                int a = st.pop();
-11                String ops = token;
-12                switch (ops) {
-13                    case "+":
-14                        st.push(a + b);
-15                        break;
-16                    case "-":
-17                        st.push(a - b);
-18                        break;
-19                    case "*":
-20                        st.push(a * b);
-21                        break;
-22                    case "/":
-23                        st.push(a / b);
-24                        break;
-25                }
-26            } else {
-27                int num = Integer.parseInt(token);
-28                st.push(num);
-29            }
-30        }
-31        return st.pop();
-32    }
-33}
+2    public List<List<Integer>> threeSum(int[] nums) {
+3        int n = nums.length;
+4        List<List<Integer>> res = new ArrayList<>();
+5        Set<List<Integer>> set = new HashSet<>();
+6
+7        Arrays.sort(nums);
+8        for (int i = 0; i < n; i++) {
+9            if (i > 0 && nums[i] == nums[i - 1])
+10                continue;
+11
+12            int l = i + 1;
+13            int r = n - 1;
+14            while (l < r) {
+15                int sum = nums[i] + nums[l] + nums[r];
+16                if (sum > 0) {
+17                    r--;
+18                } else if (sum < 0) {
+19                    l++;
+20                } else {
+21                    ArrayList<Integer> ls = 
+22                                new ArrayList<>(Arrays.asList(nums[i], nums[l], nums[r]));
+23
+24                    // Collections.sort(ls);
+25                    if (!set.contains(ls)) {
+26                        set.add(ls);
+27                        res.add(ls);
+28                    }
+29                    l++;
+30                }
+31            }
+32        }
+33        return res;
+34    }
+35}
