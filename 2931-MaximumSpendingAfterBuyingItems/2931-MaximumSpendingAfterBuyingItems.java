e1276db162b1e6bf447799006beb2a150630958d
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-06-20 23:15:29 +0530

    File updated at 20/06/2026, 23:15:29

--- 2931-MaximumSpendingAfterBuyingItems/2931-MaximumSpendingAfterBuyingItems.java
+++ 2931-MaximumSpendingAfterBuyingItems/2931-MaximumSpendingAfterBuyingItems.java
@@ -1,34 +1,28 @@
-// Last updated: 19/06/2026, 20:47:31
+// Last updated: 20/06/2026, 23:15:29
 1class Solution {
-2    public List<List<Integer>> threeSum(int[] nums) {
-3        int n = nums.length;
-4        List<List<Integer>> res = new ArrayList<>();
-5        HashSet<List<Integer>> set = new HashSet<>();
-6
-7        Arrays.sort(nums);
-8        for (int i = 0; i < n; i++) {
-9            if (i > 0 && nums[i] == nums[i - 1])
-10                continue;
-11
-12            int l = i + 1;
-13            int r = n - 1;
-14            while (l < r) {
-15                int sum = nums[i] + nums[l] + nums[r];
-16                if (sum > 0) {
-17                    r--;
-18                } else if (sum < 0) {
-19                    l++;
-20                } else {
-21                    List<Integer> ls = Arrays.asList(nums[i], nums[l], nums[r]);
-22                    Collections.sort(ls);
-23                    if (!set.contains(ls)) {
-24                        res.add(ls);
-25                        set.add(ls);
-26                    }
-27                    l++;
-28                }
-29            }
-30        }
-31        return res;
-32    }
-33}
+2    public int maxArea(int[] height) {
+3        int n = height.length;
+4        int res = 0;
+5
+6        int l = 0;
+7        int r = n - 1;
+8
+9        while (l < r) {
+10            int minh = Math.min(height[l], height[r]);
+11            int cur = minh * (r - l);
+12            System.out.println("minh=" + minh + ", l="+ l);
+13            System.out.println("cur=" + cur);
+14            if (cur > res)
+15                res = cur;
+16
+17            if (height[l] >= height[r]) {
+18                r--;
+19            } else {
+20                l++;
+21            }
+22
+23        }
+24
+25        return res;
+26    }
+27}
