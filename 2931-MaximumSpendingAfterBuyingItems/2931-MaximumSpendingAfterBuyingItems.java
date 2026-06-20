88925f67bfd926abad67a5b8375050c442ca8cb7
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-06-20 23:34:43 +0530

    File updated at 20/06/2026, 23:34:43

--- 2931-MaximumSpendingAfterBuyingItems/2931-MaximumSpendingAfterBuyingItems.java
+++ 2931-MaximumSpendingAfterBuyingItems/2931-MaximumSpendingAfterBuyingItems.java
@@ -1,28 +1,19 @@
-// Last updated: 20/06/2026, 23:15:29
+// Last updated: 20/06/2026, 23:34:43
 1class Solution {
-2    public int maxArea(int[] height) {
-3        int n = height.length;
-4        int res = 0;
-5
-6        int l = 0;
-7        int r = n - 1;
-8
-9        while (l < r) {
-10            int minh = Math.min(height[l], height[r]);
-11            int cur = minh * (r - l);
-12            System.out.println("minh=" + minh + ", l="+ l);
-13            System.out.println("cur=" + cur);
-14            if (cur > res)
-15                res = cur;
-16
-17            if (height[l] >= height[r]) {
-18                r--;
-19            } else {
-20                l++;
-21            }
-22
-23        }
-24
-25        return res;
-26    }
-27}
+2    public int maxProfit(int[] prices) {
+3        int n = prices.length;
+4        int profit = 0;
+5        int buy= prices[0];
+6        for(int i=1;i<n;i++){
+7            if(prices[i] > buy){
+8                if(prices[i] - buy > profit)
+9                    profit= prices[i] - buy;
+10            }
+11            else{
+12                buy= prices[i];
+13            }    
+14        }
+15        return profit;
+16    }
+17
+18}
