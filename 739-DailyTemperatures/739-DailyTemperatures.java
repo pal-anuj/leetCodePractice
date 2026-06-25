74a970bb156dd0746d2b5d29230a78881889b53b
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-06-25 09:57:31 +0530

    File updated at 25/06/2026, 09:57:30

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,17 +1,32 @@
-// Last updated: 16/04/2026, 05:37:00
-class Solution {
-    public int[] dailyTemperatures(int[] temp) {
-        int n = temp.length;
-        int[] res = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < n; i++) {
-
-            while (!st.isEmpty() && temp[st.peek()] < temp[i]) {
-                int idx = st.pop();
-                res[idx] = i - idx;
-            }
-            st.push(i);
-        }
-        return res;
-    }
-}
+// Last updated: 25/06/2026, 09:57:30
+1class Solution {
+2    public int[] dailyTemperatures(int[] temp) {
+3        int n = temp.length;
+4        int[] res = new int[n];
+5        Stack<Integer> st = new Stack<>();
+6        for (int i = n - 1; i >= 0; i--) {
+7            while (!st.isEmpty() && temp[st.peek()] <= temp[i]) {
+8                st.pop();
+9            }
+10
+11            if (st.isEmpty()) {
+12                res[i] = 0;
+13            } else {
+14                // System.out.println("index="+i+" curtemp="+ temp[i] +", st peek="+ temp[st.peek()]);
+15                res[i] = st.peek() - i;
+16            }
+17            st.push(i);
+18        }
+19
+20        // // brute force
+21        // for (int i = 0; i < n; i++) {
+22        //     for (int j = i; j < n; j++) {
+23        //         if (temp[i] < temp[j]) {
+24        //             res[i] = j - i;
+25        //             break;
+26        //         }
+27        //     }
+28        // }
+29        return res;
+30    }
+31}
