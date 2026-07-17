d104a144e1f8d8ac4d7c0c2669b3805d605f7c11
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-07-17 20:33:49 +0530

    File updated at 17/07/2026, 20:33:49

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,13 +1,19 @@
-// Last updated: 17/07/2026, 20:28:39
+// Last updated: 17/07/2026, 20:33:49
 1class Solution {
-2    public boolean containsDuplicate(int[] nums) {
-3        Set<Integer> set = new HashSet<>();
-4        for (int num : nums) {
-5            if (set.contains(num))
-6                return true;
-7
-8            set.add(num);
-9        }
-10        return false;
-11    }
-12}
+2    public boolean isAnagram(String s, String t) {
+3        int[] arr = new int[26];
+4        if(s.length()!= t.length())
+5            return false;
+6
+7        for (int i = 0; i < s.length(); i++) {
+8            arr[s.charAt(i) - 'a']++;
+9            arr[t.charAt(i) - 'a']--;
+10        }
+11
+12        for (int num : arr) {
+13            if (num != 0)
+14                return false;
+15        }
+16        return true;
+17    }
+18}
