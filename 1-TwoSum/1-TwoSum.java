9a637a2e03dd635e4b84ca912841894a460228d6
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-07-17 20:25:22 +0530

    File updated at 17/07/2026, 20:25:22

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,32 +1,14 @@
-// Last updated: 06/06/2026, 20:46:25
+// Last updated: 17/07/2026, 20:25:22
 1class Solution {
 2    public int[] twoSum(int[] nums, int target) {
-3        int n = nums.length;
-4        int[] res = new int[2];
-5        HashMap<Integer, Integer> map = new HashMap<>();
-6        map.put(nums[0], 0);
-7        for (int i = 1; i < n; i++) {
-8            int diff = target - nums[i];
-9            if (map.containsKey(diff)) {
-10                res[0] = map.get(diff);
-11                res[1] = i;
-12                return res;
-13            }
-14            map.put(nums[i], i);
-15        }
-16        return res;
-17
-18        // // bruet force
-19        // int[] res = new int[2];
-20        // for (int i = 0; i < n - 1; i++) {
-21        //     for (int j = i + 1; j < n; j++) {
-22        //         if (nums[i] + nums[j] == target) {
-23        //             res[0] = i;
-24        //             res[1] = j;
-25        //             return res;
-26        //         }
-27        //     }
-28        // }
-29        // return res;
-30    }
-31}
+3        HashMap<Integer, Integer> map = new HashMap<>();
+4        for (int i = 0; i < nums.length; i++) {
+5            int diff = target - nums[i];
+6            if (map.containsKey(diff)) {
+7                return new int[] { map.get(diff), i };
+8            }
+9            map.put(nums[i], i);
+10        }
+11        return new int[0];
+12    }
+13}
