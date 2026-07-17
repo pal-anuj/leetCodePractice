68632859223f364fc1d9a4ee3f0703be335828f1
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-07-17 20:28:39 +0530

    File updated at 17/07/2026, 20:28:39

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,14 +1,13 @@
-// Last updated: 17/07/2026, 20:25:22
+// Last updated: 17/07/2026, 20:28:39
 1class Solution {
-2    public int[] twoSum(int[] nums, int target) {
-3        HashMap<Integer, Integer> map = new HashMap<>();
-4        for (int i = 0; i < nums.length; i++) {
-5            int diff = target - nums[i];
-6            if (map.containsKey(diff)) {
-7                return new int[] { map.get(diff), i };
-8            }
-9            map.put(nums[i], i);
-10        }
-11        return new int[0];
-12    }
-13}
+2    public boolean containsDuplicate(int[] nums) {
+3        Set<Integer> set = new HashSet<>();
+4        for (int num : nums) {
+5            if (set.contains(num))
+6                return true;
+7
+8            set.add(num);
+9        }
+10        return false;
+11    }
+12}
