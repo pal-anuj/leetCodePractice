e5e861fa48524539000ef0b52941afaf3b428f7b
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-07-19 23:36:28 +0530

    File updated at 19/07/2026, 23:36:28

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,18 +1,26 @@
-// Last updated: 10/06/2026, 19:02:15
+// Last updated: 19/07/2026, 23:36:28
 1class Solution {
-2    public int lengthOfLongestSubstring(String s) {
-3        int res = 0;
-4        int l = 0;
-5        HashMap<Character, Integer> map = new HashMap<>();
-6        for (int r = 0; r < s.length(); r++) {
-7            char ch = s.charAt(r);
-8            if (map.containsKey(ch)) {
-9                l = Math.max(l, map.get(ch) + 1);
-10            }
-11
-12            map.put(ch, r);
-13            res = Math.max(res, r - l + 1);
-14        }
-15        return res;
-16    }
-17}
+2    public int[] topKFrequent(int[] nums, int k) {
+3
+4        Map<Integer, Integer> map = new HashMap<>();
+5        for (int num : nums) {
+6            map.put(num, map.getOrDefault(num, 0) + 1);
+7        }
+8
+9        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+10        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+11            int[] element = new int[] { entry.getKey(), entry.getValue() };
+12            pq.offer(element);
+13            if (pq.size() > k) {
+14                pq.poll();
+15            }
+16        }
+17
+18        int[] res = new int[pq.size()];
+19        int i = 0;
+20        while (!pq.isEmpty()) {
+21            res[i++] = pq.poll()[0];
+22        }
+23        return res;
+24    }
+25}
