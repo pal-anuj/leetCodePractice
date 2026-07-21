e3beea354e87a59d82df4ebfd1c6808d15b43139
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-07-21 23:57:15 +0530

    File updated at 21/07/2026, 23:57:15

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,19 +1,14 @@
-// Last updated: 17/07/2026, 20:38:16
+// Last updated: 21/07/2026, 23:57:15
 1class Solution {
-2    public boolean canConstruct(String ransomNote, String magazine) {
-3
-4        int[] arr = new int[26];
-5        for (char c : magazine.toCharArray()) {
-6            arr[c - 'a']++;
-7        }
-8
-9        for (char c : ransomNote.toCharArray()) {
-10            if (arr[c - 'a'] > 0) {
-11                arr[c - 'a']--;
-12            } else
-13                return false;
-14        }
-15
-16        return true;
-17    }
-18}
+2    public int[] twoSum(int[] nums, int target) {
+3        HashMap<Integer, Integer> map = new HashMap<>();
+4        for (int i = 0; i < nums.length; i++) {
+5            int diff = target - nums[i];
+6            if (map.containsKey(diff)) {
+7                return new int[] { map.get(diff), i };
+8            }
+9            map.put(nums[i], i);
+10        }
+11        return new int[0];
+12    }
+13}
