556033c567f61ab4c8048329dfc1bf3c71a2aafc
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-07-25 00:13:39 +0530

    File updated at 25/07/2026, 00:13:38

--- 451-SortCharactersByFrequency/451-SortCharactersByFrequency.java
+++ 451-SortCharactersByFrequency/451-SortCharactersByFrequency.java
@@ -1,26 +1,22 @@
-// Last updated: 05/12/2025, 08:25:16
+// Last updated: 25/07/2026, 00:13:38
 1class Solution {
 2    public String frequencySort(String s) {
-3        
-4        // 1. Count frequencies
-5        Map<Character, Integer> freq= new HashMap<>();
-6        for(char c : s.toCharArray()){
-7            freq.put(c, freq.getOrDefault(c, 0)+1);
-8        }
-9
-10        // 2. Convert to list of map entries
-11        List<Map.Entry<Character, Integer>> list= new ArrayList<>(freq.entrySet());
-12        // 3. Sort by decreasing frequency
-13        list.sort((a,b) -> b.getValue()- a.getValue());
-14
-15        // 4. Build result string
-16        StringBuilder sb= new StringBuilder();
-17        for(Map.Entry<Character, Integer> e : list){
-18            char ch= e.getKey();
-19            int count= e.getValue();
-20            sb.append(String.valueOf(ch).repeat(count));
-21        }
-22        
-23        return sb.toString();
-24    }
-25}
+3
+4        Map<Character, Integer> map = new HashMap<>();
+5        for (char c : s.toCharArray()) {
+6            map.put(c, map.getOrDefault(c, 0) + 1);
+7        }
+8
+9        List<Map.Entry<Character,Integer>> freqMapList = new ArrayList<>(map.entrySet());
+10        freqMapList.sort((a,b) -> b.getValue() - a.getValue());
+11
+12        StringBuilder sb = new StringBuilder();
+13        for (Map.Entry<Character, Integer> e : freqMapList) {
+14            char ch=  e.getKey();
+15            int count= e.getValue();
+16            sb.append(String.valueOf(ch).repeat(count));
+17        }
+18
+19        return sb.toString();
+20    }
+21}
