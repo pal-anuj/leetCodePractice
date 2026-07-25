b5b7ea944dabb2a672d7824eb5fd5a4264e87abe
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-07-25 14:43:03 +0530

    File updated at 25/07/2026, 14:43:03

--- 125-ValidPalindrome/125-ValidPalindrome.java
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -1,26 +1,47 @@
-// Last updated: 25/07/2026, 14:18:42
+// Last updated: 25/07/2026, 14:43:03
 1class Solution {
-2    public List<List<Integer>> threeSum(int[] nums) {
-3        int n = nums.length;
-4        List<List<Integer>> res = new ArrayList<>();
-5        Set<List<Integer>> set = new HashSet<>();
-6        for (int i = 0; i < n - 2; i++) {
-7            int target = 0 - nums[i];
-8            Map<Integer, Integer> idxMap = new HashMap<>();
-9            for (int j = i + 1; j < n; j++) {
-10                int diff = target - nums[j];
-11                if (idxMap.containsKey(diff)) {
-12                    List<Integer> ls = new ArrayList<>(Arrays.asList(nums[i], nums[j], diff));
-13                    ls.sort(Integer::compareTo);
-14                    if (!set.contains(ls)) {
-15                        res.add(new ArrayList<>(ls));
-16                        set.add(ls);
-17                    }
-18                }
-19                idxMap.put(nums[j], j);
-20            }
-21        }
-22
-23        return res;
-24    }
-25}
+2
+3    // [-4,-1,-1,0,1,2]
+4    public List<List<Integer>> threeSum(int[] nums) {
+5        int n = nums.length;
+6        Arrays.sort(nums);
+7        List<List<Integer>> res = new ArrayList<>();
+8        for (int i = 0; i < n - 2; i++) {
+9
+10            // Skip duplicate first elements
+11            if (i > 0 && nums[i] == nums[i - 1]) {
+12                continue;
+13            }
+14            int l = i + 1;
+15            int r = n - 1;
+16            while (l < r) {
+17                int sum = nums[l] + nums[r] + nums[i];
+18                if (sum < 0) {
+19                    l++;
+20                } 
+21                else if (sum > 0) {
+22                    r--;
+23                } 
+24                else if (sum == 0) {
+25                    List<Integer> ls = new ArrayList<>(Arrays.asList(nums[l], nums[r], nums[i]));
+26                    res.add(ls);
+27                    l++;
+28                    r--;
+29
+30                    // Skip duplicate left values
+31                    while (l < r && nums[l] == nums[l - 1]) {
+32                        l++;
+33                    }
+34
+35                    // Skip duplicate right values
+36                    while (l < r && nums[r] == nums[r + 1]) {
+37                        r--;
+38                    }
+39                }
+40            }
+41
+42        }
+43
+44        return res;
+45    }
+46}
