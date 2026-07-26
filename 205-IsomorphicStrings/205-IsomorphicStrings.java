c9393fcc99b968a20f814d253fa712f1438af9dc
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-07-26 23:18:49 +0530

    File updated at 26/07/2026, 23:18:49

--- 205-IsomorphicStrings/205-IsomorphicStrings.java
+++ 205-IsomorphicStrings/205-IsomorphicStrings.java
@@ -1,19 +1,20 @@
-// Last updated: 26/07/2026, 21:43:23
+// Last updated: 26/07/2026, 23:18:49
 1class Solution {
 2    public int subarraySum(int[] nums, int k) {
-3        int n= nums.length;
-4        int res=0;
-5        int prefix=0;
-6        
-7        for(int i=0;i<n;i++){
-8            int sum=0;
-9            for(int j=i;j<n;j++){
-10                sum+= nums[j];
-11                if(sum==k){
-12                    res++;
-13                }
-14            }
+3        int n = nums.length;
+4        Map<Integer, Integer> map = new HashMap<>();
+5        map.put(0, 1);
+6
+7        int prefix = 0;
+8        int count = 0;
+9        for (int num : nums) {
+10            prefix += num;
+11            if (map.containsKey(prefix - k)) {
+12                count += map.get(prefix - k);
+13            }
+14            map.put(prefix, map.getOrDefault(prefix, 0) + 1);
 15        }
-16        return res;
-17    }
-18}
+16
+17        return count;
+18    }
+19}
