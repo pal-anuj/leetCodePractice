df253a89665ec543a87d6e5108ecdb33ae464ed6
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-07-27 09:45:38 +0530

    File updated at 27/07/2026, 09:45:38

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,40 +1,23 @@
-// Last updated: 18/06/2026, 23:24:45
+// Last updated: 27/07/2026, 09:45:38
 1class Solution {
-2    public int longestSubstring(String s, int k) {
-3        if (s.length() < k)
-4            return 0;
-5
-6        int[] freq = new int[26];
-7        for (int i = 0; i < s.length(); i++) {
-8            freq[s.charAt(i) - 'a']++;
-9        }
-10
-11        for (int i = 0; i < s.length(); i++) {
-12            if (freq[s.charAt(i) - 'a'] < k) {
-13                return Math.max(longestSubstring(s.substring(0, i), k),
-14                        longestSubstring(s.substring(i + 1, s.length()), k));
-15            }
-16        }
-17
-18        return s.length();
-19        // // Brute force
-20        // for (int i = 0; i < s.length(); i++) {
-21        //     int[] freq = new int[26];
-22        //     for (int j = i; j < s.length(); j++) {
-23        //         freq[s.charAt(j) - 'a']++;
-24        //         boolean valid = true;
-25        //         for (int c : freq) {
-26        //             if (c > 0 && c < k) {
-27        //                 valid = false;
-28        //                 break;
-29        //             }
-30        //         }
-31
-32        //         if (valid) {
-33        //             maxlen = Math.max(maxlen, j - i + 1);
-34        //         }
-35        //     }
-36        // }
-37        // return maxlen;
-38    }
-39}
+2    public int lengthOfLongestSubstring(String s) {
+3        int n = s.length();
+4        Map<Character, Integer> map = new HashMap<>();
+5        int l = 0;
+6        int r = 0;
+7        int maxLen = 0;
+8
+9        while (r < n) {
+10            char c = s.charAt(r);
+11            if (map.containsKey(c)) {
+12                l = Math.max(l, map.get(c) + 1);
+13            }
+14
+15            if (maxLen < (r - l + 1)) {
+16                maxLen = r - l + 1;
+17            }
+18            map.put(c, r++);
+19        }
+20        return maxLen;
+21    }
+22}
