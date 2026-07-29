b53a3fb59778529c5c68447544ed29b92290c887
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-07-29 07:29:45 +0530

    File updated at 29/07/2026, 07:29:45

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,19 +1,19 @@
-// Last updated: 24/06/2026, 09:01:43
+// Last updated: 29/07/2026, 07:29:45
 1class Solution {
 2    public boolean isValid(String s) {
 3        Stack<Character> st = new Stack<>();
-4        for (char c : s.toCharArray()) {
-5            if (c == '(')
-6                st.push(')');
-7            else if (c == '[')
-8                st.push(']');
-9            else if (c == '{')
-10                st.push('}');
-11            else {
-12                if (st.isEmpty() || st.pop() != c)
-13                    return false;
-14            }
+4
+5        for(char ch : s.toCharArray()){
+6            if(ch =='(')
+7                st.push(')');
+8            else if(ch=='{')
+9                st.push('}');
+10            else if(ch=='[')
+11                st.push(']');
+12            else if(!st.isEmpty() && ch == st.peek())
+13                st.pop();    
+14            else return false;
 15        }
-16        return st.isEmpty();
+16        return st.isEmpty()? true: false;
 17    }
 18}
