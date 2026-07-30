5b743a3825bafb6c789b8ff8ae6af50c8f646288
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-07-30 05:30:02 +0530

    File updated at 30/07/2026, 05:30:01

--- 155-MinStack/155-MinStack.java
+++ 155-MinStack/155-MinStack.java
@@ -1,35 +1,49 @@
-// Last updated: 07/02/2026, 00:36:25
-class MinStack {
-	private Node head;
-        
-    public void push(int x) {
-        if (head == null) 
-            head = new Node(x, x, null);
-        else 
-            head = new Node(x, Math.min(x, head.min), head);
-    }
-    
-    public void pop() {
-        head = head.next;
-    }
-    
-    public int top() {
-        return head.val;
-    }
-    
-    public int getMin() {
-        return head.min;
-    }
-        
-    private class Node {
-        int val;
-        int min;
-        Node next;
-            
-        private Node(int val, int min, Node next) {
-            this.val = val;
-            this.min = min;
-            this.next = next;
-        }
-    }
-}
+// Last updated: 30/07/2026, 05:30:01
+1class MinStack {
+2
+3    static class Node{
+4        int val;
+5        int min;
+6        Node next;
+7
+8        Node(int val, int min, Node next){
+9            this.val=val;
+10            this.min=min;
+11            this.next=next;
+12        }
+13    }
+14
+15    Node head;
+16    public MinStack() {
+17    }
+18    
+19    public void push(int value) {
+20        if(head==null){
+21            head= new Node(value, value, null);
+22        }
+23        else{
+24            head= new Node(value, Math.min(head.min, value), head);
+25        }
+26    }
+27    
+28    public void pop() {
+29        head= head.next;
+30    }
+31    
+32    public int top() {
+33        return head.val;
+34    }
+35    
+36    public int getMin() {
+37        return head.min;
+38    }
+39}
+40
+41/**
+42 * Your MinStack object will be instantiated and called as such:
+43 * MinStack obj = new MinStack();
+44 * obj.push(value);
+45 * obj.pop();
+46 * int param_3 = obj.top();
+47 * int param_4 = obj.getMin();
+48 */
