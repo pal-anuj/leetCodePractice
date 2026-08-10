84ae5ef553aef9fb219e96c4bf9a9cc6c6514512
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-08-10 07:57:27 +0530

    File updated at 10/08/2026, 07:57:27

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,37 +1,22 @@
-// Last updated: 08/04/2026, 10:00:45
+// Last updated: 10/08/2026, 07:57:27
 1class Solution {
 2    public int maxArea(int[] height) {
-3        int n = height.length;
-4        int res = 0;
-5
-6        int l = 0;
-7        int r = n - 1;
-8
-9        // Efficinet apprach using 2 pointer
-10        while (l < r) {
-11            int minh = Math.min(height[l], height[r]);
-12            int cur = (r - l) * minh;
-13            res = Math.max(res, cur);
-14
-15            if (height[l] < height[r])
-16                l++;
-17            else if (height[l] > height[r])
-18                r--;
-19            else {
-20                l++;
-21                r--;
-22            }
-23        }
-24
-25        // //    naive approach
-26        //     int cur = 0;
-27        // for (int i = 0; i < n - 1; i++) {
-28        //     for (int j = i + 1; j < n; j++) {
-29        //         int minh = Math.min(height[i], height[j]);
-30        //         int cur = (j - i) * minh;
-31        //         res = Math.max(res, cur);
-32        //     }
-33        // }
-34        return res;
-35    }
-36}
+3        int n= height.length-1;
+4        int res=0;
+5        int l=0;
+6        int r=n;
+7
+8        while(l<r){
+9            if(height[l] < height[r]){
+10                int minh= Math.min(height[l], height[r]);
+11                res= Math.max( res ,minh*(r-l));
+12                l++;
+13            }else{
+14                int minh= Math.min(height[l], height[r]);
+15                res= Math.max( res ,minh*(r-l));
+16                r--;
+17            }
+18        }
+19        return res;
+20    }
+21}
