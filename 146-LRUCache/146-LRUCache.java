611ec52a8c1f00134b140f85b4609d32f2feea82
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-08-11 06:59:07 +0530

    File updated at 11/08/2026, 06:59:06

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,85 +1,87 @@
-// Last updated: 11/12/2025, 09:52:29
+// Last updated: 11/08/2026, 06:59:06
 1class LRUCache {
 2    class Node{
-3        int key, value;
-4        Node next,prev;
-5        Node(int k, int v){
-6            key= k;
-7            value=v;
-8        }
-9    }
-10    HashMap<Integer, Node> cache;
-11    Node head,tail;
+3        int key;
+4        int val;
+5        Node next, prev;
+6        Node(int key, int val){
+7            this.key=key;
+8            this.val=val;
+9        }
+10    }
+11
 12    int capacity;
-13    int size=0;
-14
-15    public LRUCache(int cap) {
-16        capacity=cap;
-17        cache= new HashMap<>();
-18        head= new Node(0,0);
-19        tail= new Node(0,0);
-20        head.next= tail;
-21        tail.prev= head;
-22    }
-23    
-24    public void moveToHead(Node node){
-25        remove(node);
-26        insert(node);            
-27    }
-28    // 2
-29    // 1->2->3
-30    public void remove(Node node){
-31        node.next.prev=node.prev;
-32        node.prev.next=node.next; 
+13    int size;
+14    Map<Integer,Node> cache;
+15    Node head,tail;
+16
+17
+18    public LRUCache(int capacity) {
+19        cache= new HashMap<>();
+20        size=0;
+21        this.capacity= capacity;
+22        head= new Node(0,0);
+23        tail= new Node(0,0);
+24        head.next=tail;
+25        tail.prev=head;
+26    }
+27    
+28    public int get(int key) {
+29        Node node=cache.get(key);
+30        if(node==null) return -1;
+31        moveToHead(node);
+32        return node.val;
 33    }
-34    // 1
-35    // 0->2->3->4
-36    public void insert(Node node){
-37        node.next= head.next;            
-38        node.prev=head;
-39        head.next.prev= node;
-40        head.next=node;
-41    }
-42
-43    // 0->1->2->3->4->5->0
-44    public Node poptail(){
-45        Node res= tail.prev;
-46        remove(res);
-47        return res;
-48    }
-49
-50    public int get(int key) {
-51        Node node= cache.get(key);
-52        if(node==null) return -1;
-53        moveToHead(node);
-54        return node.value;
-55    }
-56    
-57    public void put(int key, int value) {
-58        Node node= cache.get(key);
-59
-60        if(node!=null){
-61            node.value=value;
-62            moveToHead(node);
-63        }
-64        else{
-65            node= new Node(key, value);
-66            insert(node);
-67            cache.put(key, node);
-68            size++;
-69
-70            if(size>capacity){
-71                Node tailNode= poptail();
-72                cache.remove(tailNode.key);  
-73                size--;              
-74            }
-75        } 
+34
+35    private void moveToHead(Node node){
+36        remove(node);
+37        insert(node);
+38    }
+39
+40    private void remove(Node node){
+41        node.prev.next=node.next;
+42        node.next.prev= node.prev;
+43    }
+44    private void insert(Node node){
+45        node.next= head.next;
+46        node.prev= head;
+47        head.next.prev= node;
+48        head.next= node;
+49    }
+50    
+51    public void put(int key, int value) {
+52        Node node= cache.get(key);
+53
+54        if(node!=null){
+55            node.val= value;
+56            moveToHead(node);
+57        }
+58        else{
+59            node= new Node(key, value);
+60            insert(node);
+61            cache.put(key, node);
+62            size++;
+63
+64            if(capacity < size){
+65                Node tailNode= poptail();
+66                cache.remove(tailNode.key);
+67                size--;
+68            }
+69        }
+70    }
+71
+72    private Node poptail(){
+73        Node res= tail.prev;
+74        remove(res);
+75        return res;
 76    }
-77}
+77
 78
-79/**
-80 * Your LRUCache object will be instantiated and called as such:
-81 * LRUCache obj = new LRUCache(capacity);
-82 * int param_1 = obj.get(key);
-83 * obj.put(key,value);
-84 */
+79}
+80
+81/**
+82 * Your LRUCache object will be instantiated and called as such:
+83 * LRUCache obj = new LRUCache(capacity);
+84 * int param_1 = obj.get(key);
+85 * obj.put(key,value);
+86 */
