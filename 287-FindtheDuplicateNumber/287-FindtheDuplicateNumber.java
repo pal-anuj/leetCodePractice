921dbb63b0dbad9d40261d4706eba784d2ee1747
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-08-22 00:52:04 +0530

    File updated at 22/08/2026, 00:52:04

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
@@ -1,14 +1,24 @@
-// Last updated: 20/12/2025, 20:02:24
-class Solution {
-    public int findDuplicate(int[] nums) {
-        boolean[] flag=new boolean[nums.length];
-        for(int i=0;i<nums.length;i++){
-            int n=nums[i];
-            if(flag[n]){
-                return n;
-            }
-            flag[n]=true;
-        }
-        return -1;
-    }
-}
+// Last updated: 22/08/2026, 00:52:04
+1class Solution {
+2    public int findDuplicate(int[] nums) {
+3        int n = nums.length;
+4        int i = 0;
+5        while (i < n) {
+6
+7            int correctIndex = nums[i] - 1;
+8            if (nums[i] != nums[correctIndex]) { // if element is not its position correct it
+9                int temp = nums[i];
+10                nums[i] = nums[correctIndex];
+11                nums[correctIndex] = temp;
+12            } else {
+13                i++;
+14            }
+15        }
+16
+17        for (i = 0; i < n; i++) {
+18            if (nums[i] != i + 1)
+19                return nums[i];
+20        }
+21        return -1;
+22    }
+23}
