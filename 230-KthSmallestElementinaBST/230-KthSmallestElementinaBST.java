c3598e3c4ef24c1e915345d94918cab4fdd1bbf2
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-08-30 22:15:05 +0530

    File updated at 30/08/2026, 22:15:04

--- 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.java
+++ 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.java
@@ -1,4 +1,4 @@
-// Last updated: 19/01/2026, 23:39:50
+// Last updated: 30/08/2026, 22:15:04
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -14,29 +14,27 @@
 13 *     }
 14 * }
 15 */
-16class Solution {
-17
-18    int prev = 0;
-19
-20    public int kthSmallest(TreeNode root, int k) {
-21        if (root == null)
-22            return -1;
-23
-24        if (root.left != null) {
-25            int leftAns = kthSmallest(root.left, k);
-26            if (leftAns != -1)
-27                return leftAns;
-28        }
-29
-30        prev++;
-31        if (prev == k)
-32            return root.val;
-33
-34        if (root.right != null) {
-35            int rightAns = kthSmallest(root.right, k);
-36            if (rightAns != -1)
-37                return rightAns;
-38        }
-39        return -1;
-40    }
-41}
+16
+17class Solution {
+18    int preOrder=0;
+19    public int kthSmallest(TreeNode root, int k) {
+20        return kthSmallestDFS(root, k);
+21    }
+22
+23    public int kthSmallestDFS(TreeNode root, int k) {
+24        if (root == null)
+25            return -1;
+26
+27        if (root.left != null) {
+28            int left = kthSmallestDFS(root.left, k);
+29            if (left != -1)
+30                return left;
+31        }
+32        preOrder++;
+33        if (preOrder == k) {
+34            return root.val;
+35        }
+36
+37        return kthSmallestDFS(root.right, k);
+38    }
+39}
