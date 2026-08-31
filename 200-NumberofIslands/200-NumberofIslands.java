2cf222cd110199b6eeeb4767f8701006461264ad
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-08-31 08:12:39 +0530

    File updated at 31/08/2026, 08:12:39

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,47 +1,45 @@
-// Last updated: 04/03/2026, 00:47:32
+// Last updated: 31/08/2026, 08:12:39
 1class Solution {
-2    public static int[][] dir = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
-3
-4    public int numIslands(char[][] grid) {
-5
-6        int n = grid.length;
-7        int m = grid[0].length;
-8        int count = 0;
-9
-10        boolean[][] vis = new boolean[n][m];
-11        for (int i = 0; i < n; i++) {
-12            for (int j = 0; j < m; j++) {
-13                if (grid[i][j] == '1' && !vis[i][j]) {
-14                    BFS(i, j, vis, grid);
-15                    count++;
-16                }
-17            }
-18        }
-19        return count;
-20    }
-21
-22    public void BFS(int i, int j, boolean[][] vis, char[][] grid) {
-23
-24        int n = grid.length;
-25        int m = grid[0].length;
-26        Queue<int[]> q = new LinkedList<>();
-27        q.add(new int[] { i, j });
-28        vis[i][j] = true;
-29
-30        while (!q.isEmpty()) {
+2    public int numIslands(char[][] grid) {
+3        int n= grid.length;
+4        int m= grid[0].length;
+5        int count=0;
+6        boolean[][] vis= new boolean[n][m];
+7        
+8        for(int i=0;i<n;i++){
+9            for(int j=0;j<m;j++){
+10                if(grid[i][j]=='1' && !vis[i][j]){
+11                    bfs(i, j, grid, vis);
+12                    count++;
+13                }
+14            }
+15        }
+16        return count;
+17    }
+18
+19    private void bfs(int row, int col, char[][] grid, boolean[][] vis){
+20        int n=grid.length;
+21        int m= grid[0].length;
+22
+23        Queue<int[]> q= new LinkedList<>();
+24        q.add(new int[]{row, col});
+25        vis[row][col]=true;
+26
+27        int[][] dirc= {{1,0}, {-1,0}, {0,1}, {0,-1}};
+28
+29        while(!q.isEmpty()){
+30            int[] cell= q.poll();
 31
-32            int cell[] = q.poll();
-33            int row = cell[0];
-34            int col = cell[1];
-35            for (int[] d : dir) {
-36                int nr = row + d[0];
-37                int nc = col + d[1];
-38
-39                if (nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == '1' && !vis[nr][nc]) {
-40                    q.add(new int[] { nr, nc });
-41                    vis[nr][nc] = true;
-42                }
-43            }
-44        }
-45    }
-46}
+32            for(int[] dir : dirc){
+33                int nx= cell[0] + dir[0];
+34                int ny= cell[1] + dir[1];
+35
+36                if(nx >= 0 && ny >= 0 && nx < n && ny < m 
+37                    && grid[nx][ny]=='1' && !vis[nx][ny]){
+38                    q.add(new int[]{nx, ny});
+39                    vis[nx][ny]= true;
+40                }
+41            }
+42        }
+43    }
+44}
