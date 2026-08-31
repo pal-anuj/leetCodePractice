17feee900a20f00e18a701028e0a192bb6015c2c
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-08-31 05:58:30 +0530

    File updated at 31/08/2026, 05:58:30

--- 653-TwoSumIV-InputisaBST/653-TwoSumIV-InputisaBST.java
+++ 653-TwoSumIV-InputisaBST/653-TwoSumIV-InputisaBST.java
@@ -1,37 +1,33 @@
-// Last updated: 17/09/2025, 09:51:50
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean findTarget(TreeNode root, int k) {
-
-        Set<Integer> set= new HashSet<>();
-        return findTarget2(root, k, set);
-        
-    }
-
-    public boolean findTarget2(TreeNode root, int k, Set<Integer> set){
-        if(root==null)
-            return false;
-
-        if(set.contains(k-root.val))
-            return true;
-
-        set.add(root.val);    
-
-        //DFS -> calling recusion with left element || right element
-        return findTarget2(root.left, k,set) || findTarget2(root.right, k, set);
-    }
-}
+// Last updated: 31/08/2026, 05:58:30
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean findTarget(TreeNode root, int k) {
+18        HashSet<Integer> set= new HashSet<>();
+19        return findTargetDFS(root, set, k);
+20    }
+21
+22    public boolean findTargetDFS(TreeNode root, HashSet<Integer> set, int k){
+23        if(root==null) return false;
+24
+25        if(set.contains(k-root.val)) return true;
+26        set.add(root.val);
+27
+28        return findTargetDFS(root.left, set, k) || 
+29            findTargetDFS(root.right, set, k);
+30        
+31    }
+32}
