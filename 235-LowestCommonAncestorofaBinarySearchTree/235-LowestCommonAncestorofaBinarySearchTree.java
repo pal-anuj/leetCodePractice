bcd58d8e7928ecb06b145bc6d261f1d882e5df59
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-08-31 05:41:04 +0530

    File updated at 31/08/2026, 05:41:04

--- 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.java
+++ 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.java
@@ -1,4 +1,4 @@
-// Last updated: 31/08/2026, 05:36:45
+// Last updated: 31/08/2026, 05:41:04
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -11,14 +11,30 @@
 10
 11class Solution {
 12    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-13        if (root == null) return null;
-14
-15        if (root.val > p.val && root.val > q.val) {
-16            return lowestCommonAncestor(root.left, p, q);
-17        }
-18        if (root.val < p.val && root.val < q.val) {
-19            return lowestCommonAncestor(root.right, p, q);
-20        }
-21        return root;
-22    }
-23}
+13        while (root != null) {
+14            // Both nodes are in the left subtree
+15            if (root.val > p.val && root.val > q.val) {
+16                root= root.left;
+17            } 
+18            // Both nodes are in the right subtree
+19            else if (root.val < p.val && root.val < q.val) {
+20                root= root.right;
+21            } 
+22             // They split here, or root is p/q
+23            else
+24                return root;
+25        }
+26        return null;
+27
+28        // // Using Recursion, Time: O(n), SPace: O(n) stack space
+29        // if (root == null) return null;
+30
+31        // if (root.val > p.val && root.val > q.val) {
+32        //     return lowestCommonAncestor(root.left, p, q);
+33        // }
+34        // if (root.val < p.val && root.val < q.val) {
+35        //     return lowestCommonAncestor(root.right, p, q);
+36        // }
+37        // return root;
+38    }
+39}
