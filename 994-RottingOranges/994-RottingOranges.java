1166dfd2683df052c99d38f2ccdfd37f46265093
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-09-03 07:52:05 +0530

    File updated at 03/09/2026, 07:52:05

--- 994-RottingOranges/994-RottingOranges.java
+++ 994-RottingOranges/994-RottingOranges.java
@@ -1,45 +1,36 @@
-// Last updated: 02/09/2026, 23:43:11
+// Last updated: 03/09/2026, 07:52:05
 1class Solution {
-2    public int orangesRotting(int[][] grid) {
-3        int n = grid.length;
-4        int m = grid[0].length;
-5
-6        int fresh = 0;
-7        Queue<int[]> q = new LinkedList<>();
-8        for (int i = 0; i < n; i++) {
-9            for (int j = 0; j < m; j++) {
-10                if (grid[i][j] == 2) {
-11                    q.add(new int[] { i, j });
-12                } else if (grid[i][j] == 1) {
-13                    fresh++;
-14                }
-15            }
-16        }
+2    public boolean validPath(int n, int[][] edges, int source, int destination) {
+3        if(edges.length==0) return true;
+4        List<List<Integer>> graph = new ArrayList<>();
+5        for (int i = 0; i < n; i++) {
+6            graph.add(new ArrayList<>());
+7        }
+8        for (int[] edge : edges) {
+9            int u = edge[0];
+10            int v = edge[1];
+11            graph.get(u).add(v);
+12            graph.get(v).add(u);
+13        }
+14
+15        Queue<Integer> q = new LinkedList<>();
+16        boolean[] vis = new boolean[n];
 17
-18        int time = 0;
-19        int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-20        while (!q.isEmpty() && fresh > 0) {
-21            int size = q.size();
-22            time++;
-23            for (int i = 0; i < size; i++) {
-24                int[] cur = q.poll();
-25                for (int[] dir : dirs) {
-26                    int nr = cur[0] + dir[0];
-27                    int nc = cur[1] + dir[1];
-28
-29                    if (nr >= 0 && nc >= 0 && nr < n && nc < m &&
-30                            grid[nr][nc] == 1) {
-31                        q.offer(new int[] { nr, nc });
-32                        grid[nr][nc] = 2;
-33                        fresh--;
-34                    }
-35
-36                }
-37            }
-38
-39            
-40        }
-41
-42        return fresh == 0 ? time : -1;
-43    }
-44}
+18        q.offer(source);
+19        vis[source] = true;
+20
+21        while (!q.isEmpty()) {
+22
+23            int u = q.poll();
+24            for (int v : graph.get(u)) {
+25                if (v == destination)
+26                    return true;
+27                if (!vis[v]) {
+28                    vis[v] = true;
+29                    q.offer(v);
+30                }
+31            }
+32        }
+33        return false;
+34    }
+35}
