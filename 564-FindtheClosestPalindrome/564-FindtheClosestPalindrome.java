3d0c0b4ce58e41afabe7df20d4b5ce4155716b54
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-09-05 12:52:48 +0530

    File updated at 05/09/2026, 12:52:48

--- 564-FindtheClosestPalindrome/564-FindtheClosestPalindrome.java
+++ 564-FindtheClosestPalindrome/564-FindtheClosestPalindrome.java
@@ -1,51 +1,39 @@
-// Last updated: 05/09/2026, 12:47:22
-1class Solution {
-2    // Any closest palindrome must come from one of these 5 candidates:
-3    // 1. 10^(len-1) - 1
-4    // 2. 10^len + 1
-5    // 3. Palindrome made by mirroring the prefix
-6    // 4. Palindrome made by mirroring prefix - 1
-7    // 5. Palindrome made by mirroring prefix + 1
-8    public String nearestPalindromic(String n) {
-9        long num = Long.parseLong(n);
-10        int len = n.length();
-11
-12        Set<Long> candidates = new HashSet<>();
-13        candidates.add((long) Math.pow(10, len - 1) - 1);
-14        candidates.add((long) Math.pow(10, len) + 1);
-15
-16        int prefixlen = (len + 1) / 2;
-17
-18        long prefix = Long.parseLong(n.substring(0, prefixlen));
-19        candidates.add(makePalindrom(prefix, len));
-20        candidates.add(makePalindrom(prefix - 1, len));
-21        candidates.add(makePalindrom(prefix + 1, len));
-22
-23        long res = -1;
-24        for (long candidate : candidates) {
-25
-26            if (candidate == num) {
-27                continue;
-28            }
-29            long dif = Math.abs(num - candidate);
-30            if (res == -1 || dif < Math.abs(num - res) || (dif == Math.abs(num - res) && candidate < res)) {
-31                res = candidate;
-32            }
-33        }
-34        System.out.println(candidates);
-35        return String.valueOf(res);
-36
-37    }
-38
-39    private long makePalindrom(long prefix, int len) {
-40        String left = String.valueOf(prefix);
-41
-42        int st = len % 2 == 0 ? left.length() - 1 : left.length() - 2;
-43        StringBuilder sb = new StringBuilder(left);
-44
-45        for (int i = st; i >= 0; i--) {
-46            sb.append(left.charAt(i));
-47        }
-48        return Long.parseLong(sb.toString());
-49    }
-50}
+// Last updated: 05/09/2026, 12:52:48
+class Solution {
+
+    public String nearestPalindromic(String n) {
+        int len = n.length();
+        int i = len % 2 == 0 ? len / 2 - 1 : len / 2;
+        long firstHalf = Long.parseLong(n.substring(0, i + 1));
+        List<Long> possibilities = new ArrayList<>();
+
+        possibilities.add(halfToPalindrome(firstHalf, len % 2 == 0));
+        possibilities.add(halfToPalindrome(firstHalf + 1, len % 2 == 0));
+        possibilities.add(halfToPalindrome(firstHalf - 1, len % 2 == 0));
+        possibilities.add((long) Math.pow(10, len - 1) - 1);
+        possibilities.add((long) Math.pow(10, len) + 1);
+
+        long diff = Long.MAX_VALUE, res = 0, nl = Long.parseLong(n);
+        for (long cand : possibilities) {
+            if (cand == nl) continue;
+            if (Math.abs(cand - nl) < diff) {
+                diff = Math.abs(cand - nl);
+                res = cand;
+            } else if (Math.abs(cand - nl) == diff) {
+                res = Math.min(res, cand);
+            }
+        }
+
+        return String.valueOf(res);
+    }
+
+    private long halfToPalindrome(long left, boolean even) {
+        long res = left;
+        if (!even) left = left / 10;
+        while (left > 0) {
+            res = res * 10 + (left % 10);
+            left /= 10;
+        }
+        return res;
+    }
+}
