015be304e1159823e5463b4c006c0d501eb3e450
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-09-14 18:01:59 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -0,0 +1,11 @@
+// Last updated: 14/09/2026, 18:01:59
+1class Solution {
+2    public int fib(int n) {
+3        if(n==0)
+4            return 0;
+5        if(n==1)
+6            return 1;    
+7        
+8        return fib(n-1)+fib(n-2);
+9    }
+10}
