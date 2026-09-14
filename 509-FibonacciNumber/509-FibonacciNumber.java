fbd2190a2287f40f5e43a42df8fd7f22e7d19042
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-09-14 18:02:06 +0530

    File updated at 14/09/2026, 18:02:06

--- 509-FibonacciNumber/509-FibonacciNumber.java
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -1,11 +1,11 @@
-// Last updated: 14/09/2026, 18:01:59
+// Last updated: 14/09/2026, 18:02:06
 1class Solution {
 2    public int fib(int n) {
-3        if(n==0)
+3        if (n == 0)
 4            return 0;
-5        if(n==1)
-6            return 1;    
-7        
-8        return fib(n-1)+fib(n-2);
+5        if (n == 1)
+6            return 1;
+7
+8        return fib(n - 1) + fib(n - 2);
 9    }
 10}
