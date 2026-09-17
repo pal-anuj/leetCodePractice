57cca0d7a54046162866c0218a49181573022c1f
Anuj Pal <70705576+pal-anuj@users.noreply.github.com>
2026-09-17 06:15:12 +0530

    File updated at 17/09/2026, 06:15:12

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,23 +1,31 @@
-// Last updated: 27/02/2026, 05:31:34
+// Last updated: 17/09/2026, 06:15:12
 1class Solution {
-2    public List<List<Integer>> subsets(int[] nums) {
-3        List<List<Integer>> res = new ArrayList<>();
-4        backTrack(0, nums, new ArrayList<>(), res);
-5        return res;
-6    }
-7
-8    private void backTrack(int idx, int[] nums, ArrayList<Integer> curr, List<List<Integer>> res) {
-9        if (idx == nums.length) {
-10            res.add(new ArrayList<>(curr));
-11            return;
-12        }
-13
-14        // include
-15        curr.add(nums[idx]);
-16        backTrack(idx + 1, nums, curr, res);
+2
+3    Set<List<Integer>> set = new HashSet<>();
+4
+5    public List<List<Integer>> subsets(int[] nums) {
+6        List<List<Integer>> res = new ArrayList<>();
+7        List<Integer> ds = new ArrayList<>();
+8        return subsetsRec(0, nums.length, ds, res, nums);
+9    }
+10
+11    List<List<Integer>> subsetsRec(int idx, int n, List<Integer> ds, List<List<Integer>> res, int[] nums) {
+12        if (idx == n) {
+13            if (!set.contains(ds))
+14                res.add(new ArrayList<>(ds));
+15            return res;
+16        }
 17
-18        // exclude
-19        curr.remove(curr.size() - 1);
-20        backTrack(idx + 1, nums, curr, res);
-21    }
-22}
+18        // Take
+19        ds.add(nums[idx]);
+20        subsetsRec(idx + 1, n, ds, res, nums);
+21
+22        // Undo
+23        ds.remove(ds.size() - 1);
+24
+25        // Not take
+26        subsetsRec(idx + 1, n, ds, res, nums);
+27
+28        return res;
+29    }
+30}
